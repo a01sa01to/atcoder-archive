ce245f83c9131e7751c9c4a2b0e23b9e3e102165
Asa <26807394+a01sa01to@users.noreply.github.com>
2020-03-22 18:35:16 +0900

    [WA] SubmissionID: 11079795 at 2020-03-22 18:35:16 (1584869716)

---
+++ past201912-open/past201912_b_11079795_WA.cpp
@@ -0,0 +1,35 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/past201912-open/submissions/11079795
+ * Submitted at: 2020-03-22 18:35:16
+ * Problem URL: https://atcoder.jp/contests/past201912-open/tasks/past201912_b
+ * Result: WA
+ * Execution Time: 68 ms
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  int n;
+  cin >> n;
+  int a[n];
+  for(int i=0; i<n; i++){
+    cin >> a[i];
+  }
+  for(int i=1; i<n; i++){
+    int p = a[i-1];
+    int t = a[i];
+    
+    if(p>t){
+      cout << "up " << p-t;
+    }
+    if(p==t) cout << "stay";
+    if(p<t){
+      cout << "down " << t-p;
+    }
+    cout << "\n";
+  }
+  
+  return 0;
+}
