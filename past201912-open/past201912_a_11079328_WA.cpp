1353b5612666663ca33cfb33fd0c41d6d8719726
Asa <26807394+a01sa01to@users.noreply.github.com>
2020-03-22 18:14:49 +0900

    [WA] SubmissionID: 11079328 at 2020-03-22 18:14:49 (1584868489)

---
+++ past201912-open/past201912_a_11079328_WA.cpp
@@ -0,0 +1,23 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/past201912-open/submissions/11079328
+ * Submitted at: 2020-03-22 18:14:49
+ * Problem URL: https://atcoder.jp/contests/past201912-open/tasks/past201912_a
+ * Result: WA
+ * Execution Time: 1 ms
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  int a;
+  cin >> a;
+  if(a){
+    cout << a*2;
+  }
+  else{
+    cout << "error";
+  }
+  return 0;
+}
