1eb0f41cf099d1ba8f23168265d7c9415474a775
Asa <26807394+a01sa01to@users.noreply.github.com>
2020-09-17 23:08:06 +0900

    [AC] SubmissionID: 16805687 at 2020-09-17 23:08:06 (1600351686)

---
+++ abc178/abc178_a_16805687_AC.cpp
@@ -0,0 +1,19 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc178/submissions/16805687
+ * Submitted at: 2020-09-17 23:08:06
+ * Problem URL: https://atcoder.jp/contests/abc178/tasks/abc178_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  int a;
+  cin >> a;
+  cout << !a;
+  return 0;
+}
+  
