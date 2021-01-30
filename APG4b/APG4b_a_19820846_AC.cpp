1ea20d2d382e816a8ce11af21cc2c7ad7e67e5fc
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-01-30 23:02:54 +0900

    [AC] SubmissionID: 19820846 at 2021-01-30 23:02:54 (1612015374)

---
+++ APG4b/APG4b_a_19820846_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/APG4b/submissions/19820846
+ * Submitted at: 2021-01-30 23:02:54
+ * Problem URL: https://atcoder.jp/contests/APG4b/tasks/APG4b_a
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <iostream>
+using namespace std;
+int main(){
+  cout << "Hello, world!" << endl;
+  return 0;
+}
