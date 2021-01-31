16e83d47dac08fb6ab7ebcc82617ea5062007ef2
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-01-31 22:19:22 +0900

    [AC] SubmissionID: 19845118 at 2021-01-31 22:19:22 (1612099162)

---
+++ APG4b/APG4b_cu_19845118_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/APG4b/submissions/19845118
+ * Submitted at: 2021-01-31 22:19:22
+ * Problem URL: https://atcoder.jp/contests/APG4b/tasks/APG4b_cu
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  cout << "いつも" << 2525 << endl;
+  cout << "AtCoderくん" << endl;
+}
