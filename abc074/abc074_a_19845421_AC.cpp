aaa7f92ce8e3764bf2f922b2622fb122c72c064a
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-01-31 22:29:10 +0900

    [AC] SubmissionID: 19845421 at 2021-01-31 22:29:10 (1612099750)

---
+++ abc074/abc074_a_19845421_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc074/submissions/19845421
+ * Submitted at: 2021-01-31 22:29:10
+ * Problem URL: https://atcoder.jp/contests/abc074/tasks/abc074_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <iostream>
+using namespace std;
+int main(){
+  int n,a;
+  cin >> n >> a;
+  cout << n*n - a << endl;
+  return 0;
+}
