27a5aff7271d2605af532cea2c8ae13294289034
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-01-31 23:24:45 +0900

    [CE] SubmissionID: 19846895 at 2021-01-31 23:24:45 (1612103085)

---
+++ APG4b/APG4b_cn_19846895_CE.cpp
@@ -0,0 +1,27 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/APG4b/submissions/19846895
+ * Submitted at: 2021-01-31 23:24:45
+ * Problem URL: https://atcoder.jp/contests/APG4b/tasks/APG4b_cn
+ * Result: CE
+ * Execution Time: None ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int x, a, b;
+  cin >> x >> a >> b;
+
+  cout << ++x << endl;
+
+  // ここにプログラムを追記
+  x *= (a+b)
+  cout << x << endl;
+  
+  x *= x;
+  cout << x << endl;
+  cout << --x << endl;
+  return 0;
+}
