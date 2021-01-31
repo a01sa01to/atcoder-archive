ad2da7afa05e66ea57c17fa4ad30d77f254c5ac9
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-01-31 23:28:20 +0900

    [AC] SubmissionID: 19846985 at 2021-01-31 23:28:20 (1612103300)

---
+++ APG4b/APG4b_cm_19846985_AC.cpp
@@ -0,0 +1,21 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/APG4b/submissions/19846985
+ * Submitted at: 2021-01-31 23:28:20
+ * Problem URL: https://atcoder.jp/contests/APG4b/tasks/APG4b_cm
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int A, B;
+  cin >> A >> B;
+  cout << "A:";
+  for(int i=0;i<A;i++) cout << "]";
+  cout << endl << "B:";
+  for(int i=0;i<B;i++) cout << "]";
+  cout << endl;
+}
