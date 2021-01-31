85b29eb498d21edc22f796e9ca96d7dbf94f41b2
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-01-31 22:21:09 +0900

    [AC] SubmissionID: 19845174 at 2021-01-31 22:21:09 (1612099269)

---
+++ APG4b/APG4b_cs_19845174_AC.cpp
@@ -0,0 +1,22 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/APG4b/submissions/19845174
+ * Submitted at: 2021-01-31 22:21:09
+ * Problem URL: https://atcoder.jp/contests/APG4b/tasks/APG4b_cs
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  // 一年の秒数
+  int seconds = 365 * 24 * 60 * 60;
+ 
+  // 以下のコメント/* */を消して追記する
+  cout << seconds * 1 << endl;
+  cout << seconds * 2 << endl;
+  cout << seconds * 5 << endl;
+  cout << seconds * 10 << endl;
+}
