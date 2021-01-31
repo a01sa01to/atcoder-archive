02c38fa2ca18d712607a33b889406701a72c7cb7
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-01-31 22:27:43 +0900

    [AC] SubmissionID: 19845377 at 2021-01-31 22:27:43 (1612099663)

---
+++ abc076/abc076_a_19845377_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc076/submissions/19845377
+ * Submitted at: 2021-01-31 22:27:43
+ * Problem URL: https://atcoder.jp/contests/abc076/tasks/abc076_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <iostream>
+using namespace std;
+int main(){
+  int r,g;
+  cin >> r >> g;
+  cout << 2*g - r << endl;
+  return 0;
+}
