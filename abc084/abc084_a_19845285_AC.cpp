c1c6b3c01a9b0c279de56e642c6f080acf9399f3
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-01-31 22:25:07 +0900

    [AC] SubmissionID: 19845285 at 2021-01-31 22:25:07 (1612099507)

---
+++ abc084/abc084_a_19845285_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc084/submissions/19845285
+ * Submitted at: 2021-01-31 22:25:07
+ * Problem URL: https://atcoder.jp/contests/abc084/tasks/abc084_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <iostream>
+using namespace std;
+int main(){
+  int h;
+  cin >> h;
+  cout << (48-h) << endl;
+  return 0;
+}
