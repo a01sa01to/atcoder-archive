e3e51849064e7e111c9aad6f1f1ff9c0da428a09
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-01-31 22:35:10 +0900

    [AC] SubmissionID: 19845589 at 2021-01-31 22:35:10 (1612100110)

---
+++ abc082/abc082_a_19845589_AC.cpp
@@ -0,0 +1,18 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc082/submissions/19845589
+ * Submitted at: 2021-01-31 22:35:10
+ * Problem URL: https://atcoder.jp/contests/abc082/tasks/abc082_a
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+  double a,b;
+  cin >> a >> b;
+  cout << ceil((a+b)/2) << endl;
+  return 0;
+}
