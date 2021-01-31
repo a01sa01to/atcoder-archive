dd991cf0e5bd08a6f27c5990ef97b60b028fcf02
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-01-31 22:22:54 +0900

    [AC] SubmissionID: 19845220 at 2021-01-31 22:22:54 (1612099374)

---
+++ APG4b/APG4b_cr_19845220_AC.cpp
@@ -0,0 +1,18 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/APG4b/submissions/19845220
+ * Submitted at: 2021-01-31 22:22:54
+ * Problem URL: https://atcoder.jp/contests/APG4b/tasks/APG4b_cr
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <iostream>
+using namespace std;
+ 
+int main() {
+  int a,b;
+  cin >> a >> b;
+  cout << (a+b) << endl;
+  return 0;
+}
