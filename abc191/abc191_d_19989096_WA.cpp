d53b9f8c2491b8080e0735ab7199bf3face98ec7
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-02-06 22:06:34 +0900

    [WA] SubmissionID: 19989096 at 2021-02-06 22:06:34 (1612616794)

---
+++ abc191/abc191_d_19989096_WA.cpp
@@ -0,0 +1,28 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc191/submissions/19989096
+ * Submitted at: 2021-02-06 22:06:34
+ * Problem URL: https://atcoder.jp/contests/abc191/tasks/abc191_d
+ * Result: WA
+ * Execution Time: 17 ms
+ */
+
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+using ll = long long;
+
+int main(){
+  long double x,y,r;
+  cin >> x >> y >> r;
+  unsigned long long int count = 0;
+  for(ll i = ceil(x-r);i<=floor(x+r);i++){
+    long double x_i = i - x;
+    ll bottom = ceil(y - sqrt(r*r - x_i*x_i));
+    ll top = floor(y + sqrt(r*r - x_i*x_i));
+    count += top - bottom + 1;
+  }
+  cout << count << endl;
+  return 0;
+}
