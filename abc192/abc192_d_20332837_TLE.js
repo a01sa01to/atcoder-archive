51ef652299eef231aec1fc9c78851eacfc5e7a2e
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-02-20 22:28:09 +0900

    [TLE] SubmissionID: 20332837 at 2021-02-20 22:28:09 (1613827689)

---
+++ abc192/abc192_d_20332837_TLE.js
@@ -0,0 +1,38 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc192/submissions/20332837
+ * Submitted at: 2021-02-20 22:28:09
+ * Problem URL: https://atcoder.jp/contests/abc192/tasks/abc192_d
+ * Result: TLE
+ * Execution Time: 2207 ms
+ */
+
+(input=>{
+	input = input.split("\n");
+	const x = input[0];
+  const m = input[1];
+  let digit = 10;
+  for(let i=9;i>0;i--){
+    if(x.includes(String(i))){
+      digit = i;
+      break;
+    }
+  }
+  let count = 0;
+
+  while(true){
+    digit++;
+    let a = 0;
+    for(let j=0;j<x.length;j++){
+      a += x[j] * (digit**(x.length-j-1));
+    }
+
+    if(a <= parseInt(m)){
+      count++;
+    }
+    else{
+      break;
+    }
+  }
+  console.log(count);
+})(require("fs").readFileSync("/dev/stdin", "utf8"));
