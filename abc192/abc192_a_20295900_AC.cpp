ab6efd0c88e4b68d6195f4f871db8a2dfcb6b062
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-02-20 21:10:28 +0900

    [AC] SubmissionID: 20295900 at 2021-02-20 21:10:28 (1613823028)

---
+++ abc192/abc192_a_20295900_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc192/submissions/20295900
+ * Submitted at: 2021-02-20 21:10:28
+ * Problem URL: https://atcoder.jp/contests/abc192/tasks/abc192_a
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+#include <iostream>
+using namespace std;
+int main(){
+  int x;
+  cin >> x;
+  cout << (100-(x%100)) << endl;
+  return 0;
+}
