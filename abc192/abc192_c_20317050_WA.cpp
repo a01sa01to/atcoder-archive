04b137eae0d033ea5f884dc20e60c7464e85ec6a
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-02-20 21:52:11 +0900

    [WA] SubmissionID: 20317050 at 2021-02-20 21:52:11 (1613825531)

---
+++ abc192/abc192_c_20317050_WA.cpp
@@ -0,0 +1,53 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc192/submissions/20317050
+ * Submitted at: 2021-02-20 21:52:11
+ * Problem URL: https://atcoder.jp/contests/abc192/tasks/abc192_c
+ * Result: WA
+ * Execution Time: 8 ms
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+string f(string n){
+  vector<int> v;
+  int l = n.length();
+  for(int i=0;i<l;i++){
+    v.push_back((int)(n[i] - '0'));
+  }
+  vector<int> max_v,min_v;
+  sort(v.begin(),v.end());
+  min_v = v;
+  sort(v.begin(),v.end(),greater<int>());
+  max_v = v;
+
+  int max=0;
+  int min=0;
+  for(int i=0;i<l;i++){
+    max += max_v[i] * pow(10,l-i-1);
+    min += min_v[i] * pow(10,l-i-1);
+  }
+  return to_string(max-min);
+}
+
+int main(){
+  string n;
+  int k;
+  cin >> n >> k;
+  if(k==0){
+    cout << n << endl;
+    return 0;
+  }
+  else if(k==1){
+    cout << f(n) << endl;
+  }
+  else{
+    cout << f(f(n)) << endl;
+  }
+  return 0;
+}
