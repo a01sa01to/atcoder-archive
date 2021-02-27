dd3364092abe4286f3c8542c000630ee9151773e
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-02-28 00:06:39 +0900

    [AC] SubmissionID: 20558146 at 2021-02-28 00:06:39 (1614438399)

---
+++ abs/practice_1_20558146_AC.cpp
@@ -0,0 +1,19 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abs/submissions/20558146
+ * Submitted at: 2021-02-28 00:06:39
+ * Problem URL: https://atcoder.jp/contests/abs/tasks/practice_1
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int a,b,c;
+  string s;
+  cin >> a >> b >> c >> s;
+  cout << (a+b+c) << " " << s << endl;
+  return 0;
+}
