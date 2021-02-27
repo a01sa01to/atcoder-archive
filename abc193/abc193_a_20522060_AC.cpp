cf809984dcebe437df25d55577e74e4866d3d27d
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-02-27 21:03:49 +0900

    [AC] SubmissionID: 20522060 at 2021-02-27 21:03:49 (1614427429)

---
+++ abc193/abc193_a_20522060_AC.cpp
@@ -0,0 +1,19 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc193/submissions/20522060
+ * Submitted at: 2021-02-27 21:03:49
+ * Problem URL: https://atcoder.jp/contests/abc193/tasks/abc193_a
+ * Result: AC
+ * Execution Time: 4 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+  ll a,b;
+  cin >> a >> b;
+  cout << ((double)((-1)*(b-a))/a*100) << endl;
+  return 0;
+}
