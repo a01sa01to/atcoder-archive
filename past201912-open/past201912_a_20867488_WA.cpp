b0ad06435bf2ebf0f1224725879e9b86fdf38a67
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-03-13 20:16:53 +0900

    [WA] SubmissionID: 20867488 at 2021-03-13 20:16:53 (1615634213)

---
+++ past201912-open/past201912_a_20867488_WA.cpp
@@ -0,0 +1,23 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/past201912-open/submissions/20867488
+ * Submitted at: 2021-03-13 20:16:53
+ * Problem URL: https://atcoder.jp/contests/past201912-open/tasks/past201912_a
+ * Result: WA
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n;
+  cin >> n;
+  if(n){
+    cout << n*2 << endl;
+  }
+  else{
+    cout << "error" << endl;
+  }
+  return 0;
+}
