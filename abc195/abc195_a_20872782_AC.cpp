5af76b4eacd9c55d3475f9ebda31f31cbcf6c1ba
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-03-13 21:02:08 +0900

    [AC] SubmissionID: 20872782 at 2021-03-13 21:02:08 (1615636928)

---
+++ abc195/abc195_a_20872782_AC.cpp
@@ -0,0 +1,18 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc195/submissions/20872782
+ * Submitted at: 2021-03-13 21:02:08
+ * Problem URL: https://atcoder.jp/contests/abc195/tasks/abc195_a
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int m,h;
+  cin >> m >> h;
+  cout << (h%m==0?"Yes":"No") << endl;
+  return 0;
+}
