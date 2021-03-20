22a21cf6a35ee02d7f1e54ba912a59cb6c892144
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-03-20 21:00:52 +0900

    [AC] SubmissionID: 21060134 at 2021-03-20 21:00:52 (1616241652)

---
+++ abc196/abc196_a_21060134_AC.cpp
@@ -0,0 +1,18 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc196/submissions/21060134
+ * Submitted at: 2021-03-20 21:00:52
+ * Problem URL: https://atcoder.jp/contests/abc196/tasks/abc196_a
+ * Result: AC
+ * Execution Time: 3 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int a,b,c,d;
+  cin >> a >> b >> c >> d;
+  cout << (b-c) << endl;
+  return 0;
+}
