e3a4f1b14363af4fb8a4376d8aeca0613cefb9f3
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-03-27 21:02:03 +0900

    [AC] SubmissionID: 21293252 at 2021-03-27 21:02:03 (1616846523)

---
+++ abc197/abc197_a_21293252_AC.cpp
@@ -0,0 +1,18 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc197/submissions/21293252
+ * Submitted at: 2021-03-27 21:02:03
+ * Problem URL: https://atcoder.jp/contests/abc197/tasks/abc197_a
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  string s;
+  cin >> s;
+  cout << s.substr(1) << s[0] << endl;
+  return 0;
+}
