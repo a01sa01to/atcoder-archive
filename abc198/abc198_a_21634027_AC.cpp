6a6a8bb487fee8a2551dc4555e7d408ecda8ea0c
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-04-11 21:00:37 +0900

    [AC] SubmissionID: 21634027 at 2021-04-11 21:00:37 (1618142437)

---
+++ abc198/abc198_a_21634027_AC.cpp
@@ -0,0 +1,19 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc198/submissions/21634027
+ * Submitted at: 2021-04-11 21:00:37
+ * Problem URL: https://atcoder.jp/contests/abc198/tasks/abc198_a
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include <bits/stdc++.h>
+#define loop(n,i) for(int i=0;i<n;i++)
+using namespace std;
+
+int main(){
+  int n;
+  cin >> n;
+  cout << n-1 << endl;
+  return 0;
+}
