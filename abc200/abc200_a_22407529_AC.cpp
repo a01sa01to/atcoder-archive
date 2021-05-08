8ab0dc893ea355057fb325becc2b000a08d0c28c
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-05-08 21:09:58 +0900

    [AC] SubmissionID: 22407529 at 2021-05-08 21:09:58 (1620475798)

---
+++ abc200/abc200_a_22407529_AC.cpp
@@ -0,0 +1,21 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc200/submissions/22407529
+ * Submitted at: 2021-05-08 21:09:58
+ * Problem URL: https://atcoder.jp/contests/abc200/tasks/abc200_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ull = unsigned long long;
+using ull2d = vector<vector<ull>>;
+#define loop(i,n) for(ull i=0;i<n;i++)
+
+int main(){
+  double a;
+  cin >> a;
+  cout << ceil(a/100) << endl;
+  return 0;
+}
