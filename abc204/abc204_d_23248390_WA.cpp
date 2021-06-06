a75e8610aabb7f7ded89e8f56787724c489373fe
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-06-06 22:17:56 +0900

    [WA] SubmissionID: 23248390 at 2021-06-06 22:17:56 (1622985476)

---
+++ abc204/abc204_d_23248390_WA.cpp
@@ -0,0 +1,37 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc204/submissions/23248390
+ * Submitted at: 2021-06-06 22:17:56
+ * Problem URL: https://atcoder.jp/contests/abc204/tasks/abc204_d
+ * Result: WA
+ * Execution Time: 6 ms
+ */
+
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ull1d = vector<ull>;
+using ull2d = vector<ull1d>;
+using ull3d = vector<ull2d>;
+#define loop(i,n) for(ull i=0;i<n;i++)
+#define rloop(i,n) for(ll i=n-1;i>=0;i--)
+#define all(vec) vec.begin(),vec.end()
+template<class T> inline bool chmin(T& a,T b){ if(a>b){ a=b; return true;} return false; }
+template<class T> inline bool chmax(T& a,T b){ if(a>b){ a=b; return true;} return false; }
+
+int main(){ cout << fixed << setprecision(15);
+  int n;
+  cin >> n;
+  ull1d t(n);
+  loop(i,n){ cin >> t[i]; }
+  sort(all(t),greater<ull>());
+  int a=0,b=0;
+  loop(i,t.size()){
+    if(a+t[i] <= b){ a += t[i]; }
+    else{ b += t[i]; }
+  }
+  cout << max(a,b) << endl;
+  return 0;
+}
