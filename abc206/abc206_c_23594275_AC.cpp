7f00cb7c7ae9be85d36f2e1d590ce3bda581f8e2
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-06-19 21:20:59 +0900

    [AC] SubmissionID: 23594275 at 2021-06-19 21:20:59 (1624105259)

---
+++ abc206/abc206_c_23594275_AC.cpp
@@ -0,0 +1,46 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc206/submissions/23594275
+ * Submitted at: 2021-06-19 21:20:59
+ * Problem URL: https://atcoder.jp/contests/abc206/tasks/abc206_c
+ * Result: AC
+ * Execution Time: 654 ms
+ */
+
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ull> ull1d;
+typedef vector<ull1d> ull2d;
+typedef vector<ull2d> ull3d;
+#define loop(i,n) for(ull i=0;i<n;i++)
+#define rloop(i,n) for(ll i=n-1;i>=0;i--)
+#define all(vec) vec.begin(),vec.end()
+template<class T> inline bool chmin(T& a,T b){ if(a>b){ a=b; return true;} return false; }
+template<class T> inline bool chmax(T& a,T b){ if(a>b){ a=b; return true;} return false; }
+
+int main(){ cout << fixed << setprecision(15);
+  ull n;
+  cin >> n;
+  ull1d a(n), uni(0);
+  loop(i,n){ cin >> a[i]; }
+  sort(all(a));
+  ull prev = 1e10;
+  loop(i,n){
+    if(prev != a[i]){
+      uni.push_back(1);
+      prev = a[i];
+    }
+    else{
+      uni[uni.size()-1]++;
+    }
+  }
+  ull ans = 0;
+  loop(i,uni.size()){
+    ans += (n - uni[i]) * uni[i];
+  }
+  cout << ans/2 << endl;
+  return 0;
+}
