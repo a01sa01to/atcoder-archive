6c85bb5cd4b31f9aa93112b1b0ec658a796f9e85
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-06-26 22:20:57 +0900

    [WA] SubmissionID: 23794761 at 2021-06-26 22:20:57 (1624713657)

---
+++ abc207/abc207_d_23794761_WA.cpp
@@ -0,0 +1,67 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc207/submissions/23794761
+ * Submitted at: 2021-06-26 22:20:57
+ * Problem URL: https://atcoder.jp/contests/abc207/tasks/abc207_d
+ * Result: WA
+ * Execution Time: 2205 ms
+ */
+
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ull> ull1d;
+typedef vector<ull1d> ull2d;
+typedef vector<ull2d> ull3d;
+#define loop(i,n) for(ull i=0;i<n;i++)
+#define rloop(i,n) for(ll i=n-1;i>=0;i--)
+#define all(vec) vec.begin(),vec.end()
+template<class T> inline bool chmin(T& a,T b){ if(a>b){ a=b; return true;} return false; }
+template<class T> inline bool chmax(T& a,T b){ if(a>b){ a=b; return true;} return false; }
+
+const double pi = 3.141592653589793238;
+
+int main(){ cout << fixed << setprecision(15);
+  int n;
+  cin >> n;
+  // 0.2
+  vector<pair<double,double>> from(n), to(n), tmp(n);
+  loop(i,n){
+    double a,b;
+    cin >> a >> b;
+    from[i] = make_pair(a,b);
+  }
+  loop(i,n){
+    double c,d;
+    cin >> c >> d;
+    to[i] = make_pair(c,d);
+  }
+  sort(all(to));
+  bool allcan = false;
+  for(double x=-20;x<=20;x++){
+    for(double y=-20;y<=20;y++){
+      for(double theta=1;theta<360;theta++){
+        for(int i=0;i<n;i++){
+          tmp[i] = make_pair(
+            (from[i].first+x)*cos(theta*pi/180)
+             - (from[i].second+y)*sin(theta*pi/180),
+            (from[i].first+x)*sin(theta*pi/180)
+             + (from[i].second+y)*cos(theta*pi/180)
+            );
+        }
+        sort(all(tmp));
+        bool can = true;
+        for(int i=0;i<n;i++){
+          if(abs(tmp[i].first - to[i].first) > 0.2 || abs(tmp[i].second - to[i].second) > 0.2){
+            can = false;
+          }
+        }
+        if(can){ allcan = true; }
+      }
+    }
+  }
+  cout << (allcan ? "Yes" : "No") << endl;
+  return 0;
+}
