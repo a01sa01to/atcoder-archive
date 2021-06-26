e1fb921625c34a08c91b4a88d5721f4563d7b199
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-06-26 14:05:05 +0900

    [AC] SubmissionID: 23742672 at 2021-06-26 14:05:05 (1624683905)

---
+++ ahc004/ahc004_a_23742672_AC.cpp
@@ -0,0 +1,51 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/ahc004/submissions/23742672
+ * Submitted at: 2021-06-26 14:05:05
+ * Problem URL: https://atcoder.jp/contests/ahc004/tasks/ahc004_a
+ * Result: AC
+ * Execution Time: 21 ms
+ */
+
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ull> ull1d;
+typedef vector<ull1d> ull2d;
+typedef vector<ull2d> ull3d;
+#define loop(i,n) for(ull i=0;i<n;i++)
+#define rloop(i,n) for(ll i=n-1;i>=0;i--)
+#define all(vec) vec.begin(),vec.end()
+template<class T> inline bool chmin(T& a,T b){ if(a>b){ a=b; return true;} return false; }
+template<class T> inline bool chmax(T& a,T b){ if(a>b){ a=b; return true;} return false; }
+
+int main(){ cout << fixed << setprecision(15);
+  int n,m;
+  cin >> n >> m;
+  vector<string> a,b(m);
+  loop(i,m){ cin >> b[i]; }
+  sort(all(b),[](string &x, string &y){ return x.size() > y.size(); });
+  string prev = "";
+  loop(i,b.size()){
+    if(prev.size() + b[i].size() > n){
+      int l = n-prev.size();
+      auto itr = find_if(all(b),[l](string x){ return x.size() == l; });
+      if(itr == b.end()){
+        loop(j,l){ prev += "."; }
+      }
+      else{
+        prev += *itr;
+        b.erase(itr);
+      }
+      a.push_back(prev);
+      prev = "";
+    }
+    prev += b[i];
+  }
+  loop(i,n){
+    cout << a[i] << endl;
+  }
+  return 0;
+}
