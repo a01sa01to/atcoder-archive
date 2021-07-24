acc73f1a5748de6d61f986a0d269a1d4b8939fda
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-07-24 21:00:47 +0900

    [AC] SubmissionID: 24478580 at 2021-07-24 21:00:47 (1627128047)

---
+++ abc211/abc211_a_24478580_AC.cpp
@@ -0,0 +1,31 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc211/submissions/24478580
+ * Submitted at: 2021-07-24 21:00:47
+ * Problem URL: https://atcoder.jp/contests/abc211/tasks/abc211_a
+ * Result: AC
+ * Execution Time: 10 ms
+ */
+
+#define _GLIBCXX_DEBUG
+#include <atcoder/all>
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace atcoder;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ll> ll1d;
+typedef vector<ll1d> ll2d;
+
+#define loop(i, n) for (ll i = 0; i < n; i++)
+#define all(vec) vec.begin(), vec.end()
+
+int main() {
+  cout << fixed << setprecision(15);
+  double a, b;
+  cin >> a >> b;
+  cout << b + (a - b) / 3 << endl;
+  return 0;
+}
