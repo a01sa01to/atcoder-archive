2317302cdabb525aa496ba32a7e08718c261db16
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-07-31 21:32:24 +0900

    [TLE] SubmissionID: 24669623 at 2021-07-31 21:32:24 (1627734744)

---
+++ abc212/abc212_d_24669623_TLE.cpp
@@ -0,0 +1,49 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc212/submissions/24669623
+ * Submitted at: 2021-07-31 21:32:24
+ * Problem URL: https://atcoder.jp/contests/abc212/tasks/abc212_d
+ * Result: TLE
+ * Execution Time: 2205 ms
+ */
+
+#define _GLIBCXX_DEBUG
+#include <atcoder/all>
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace atcoder;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ll> ll1d;
+typedef vector<ll1d> ll2d;
+
+#define loop(i, n) for (ll i = 0; i < n; i++)
+#define all(vec) vec.begin(), vec.end()
+
+int main() {
+  cout << fixed << setprecision(15);
+  ll q;
+  cin >> q;
+  priority_queue<ll, vector<ll>, greater<ll>> pq;
+  ll add = 0;
+  loop(_, q) {
+    int a, b;
+    cin >> a;
+    if (a == 3) {
+      ll v = pq.top();
+      pq.pop();
+      cout << v + add << endl;
+      continue;
+    }
+    cin >> b;
+    if (a == 1) {
+      pq.push(b - add);
+    }
+    else {
+      add += b;
+    }
+  }
+  return 0;
+}
