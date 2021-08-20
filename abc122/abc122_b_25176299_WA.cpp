af88de57361e548cec5c916b15021fee00de2880
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-08-21 00:10:01 +0900

    [WA] SubmissionID: 25176299 at 2021-08-21 00:10:01 (1629472201)

---
+++ abc122/abc122_b_25176299_WA.cpp
@@ -0,0 +1,46 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc122/submissions/25176299
+ * Submitted at: 2021-08-21 00:10:01
+ * Problem URL: https://atcoder.jp/contests/abc122/tasks/abc122_b
+ * Result: WA
+ * Execution Time: 7 ms
+ */
+
+#include <atcoder/all>
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace atcoder;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ll> ll1d;
+typedef vector<ll1d> ll2d;
+
+#define rep(i, n) for (ll i = 0; i < n; i++)
+#define all(vec) vec.begin(), vec.end()
+
+bool chk(string s) {
+  rep(i, s.size()) {
+    if (s[i] == 'A' || s[i] == 'C' || s[i] == 'G' || s[i] == 'T') {}
+    else {
+      return false;
+    }
+  }
+  return true;
+}
+
+int main() {
+  cout << fixed << setprecision(15);
+  string s;
+  cin >> s;
+  vector<ll> ans;
+  rep(i, s.size()) rep(j, s.size() - i) {
+    string t = s.substr(i, j);
+    if (chk(t)) ans.push_back(t.size());
+  }
+  sort(all(ans), greater<ll>());
+  cout << ans[0] << endl;
+  return 0;
+}
