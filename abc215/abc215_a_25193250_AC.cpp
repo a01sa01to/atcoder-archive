20139c75b486e5c2f75b9b7e4dcad838d6d91b27
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-08-21 21:01:19 +0900

    [AC] SubmissionID: 25193250 at 2021-08-21 21:01:19 (1629547279)

---
+++ abc215/abc215_a_25193250_AC.cpp
@@ -0,0 +1,30 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc215/submissions/25193250
+ * Submitted at: 2021-08-21 21:01:19
+ * Problem URL: https://atcoder.jp/contests/abc215/tasks/abc215_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <atcoder/all>
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace atcoder;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ll> ll1d;
+typedef vector<ll1d> ll2d;
+
+#define rep(i, n) for (ll i = 0; i < n; i++)
+#define all(vec) vec.begin(), vec.end()
+
+int main() {
+  cout << fixed << setprecision(15);
+  string s;
+  cin >> s;
+  cout << ((s == "Hello,World!") ? "AC" : "WA") << endl;
+  return 0;
+}
