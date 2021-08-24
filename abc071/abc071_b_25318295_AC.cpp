c6d5ac6ee3d48976b53a6a9dd370376ad1b52154
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-08-24 23:24:10 +0900

    [AC] SubmissionID: 25318295 at 2021-08-24 23:24:10 (1629815050)

---
+++ abc071/abc071_b_25318295_AC.cpp
@@ -0,0 +1,40 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc071/submissions/25318295
+ * Submitted at: 2021-08-24 23:24:10
+ * Problem URL: https://atcoder.jp/contests/abc071/tasks/abc071_b
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+#include <atcoder/all>
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace atcoder;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ll> ll1d;
+typedef vector<ll1d> ll2d;
+
+#define rep(i, n) for (ll i = 0; i < n; i++)
+#define all(vec) vec.begin(), vec.end()
+
+int main() {
+  cout << fixed << setprecision(15);
+  string s;
+  cin >> s;
+  vector<bool> seen(26, false);
+  rep(i, s.size()) {
+    seen[s[i] - 'a'] = true;
+  }
+  rep(i, 26) {
+    if (!seen[i]) {
+      cout << char('a' + i) << endl;
+      return 0;
+    }
+  }
+  cout << "None" << endl;
+  return 0;
+}
