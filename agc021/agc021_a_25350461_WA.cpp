3a77730ab400fea3fff44b18400c7267c87be650
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-08-26 22:25:38 +0900

    [WA] SubmissionID: 25350461 at 2021-08-26 22:25:38 (1629984338)

---
+++ agc021/agc021_a_25350461_WA.cpp
@@ -0,0 +1,34 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/agc021/submissions/25350461
+ * Submitted at: 2021-08-26 22:25:38
+ * Problem URL: https://atcoder.jp/contests/agc021/tasks/agc021_a
+ * Result: WA
+ * Execution Time: 13 ms
+ */
+
+#include <atcoder/all>
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace atcoder;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ll> ll1d;
+typedef vector<ll1d> ll2d;
+typedef pair<ll, ll> P;
+
+#define rep(i, n) for (ll i = 0; i < n; i++)
+#define all(vec) vec.begin(), vec.end()
+
+int main() {
+  cout << fixed << setprecision(15);
+  ll n;
+  cin >> n;
+  long double l10 = log10(n);
+  ll l10l = l10;
+  ll tp = (n / ll(pow(10, l10l))) % 10;
+  cout << (tp - 1) + (9 * l10l) << endl;
+  return 0;
+}
