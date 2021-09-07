54923438c45e92f41007a776c8e389ab21787ffb
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-08 01:15:56 +0900

    [AC] SubmissionID: 25676808 at 2021-09-08 01:15:56 (1631031356)

---
+++ rcl-contest-2021-long/rcl_contest_2021_long_a_25676808_AC.sh
@@ -0,0 +1,10 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/rcl-contest-2021-long/submissions/25676808
+ * Submitted at: 2021-09-08 01:15:56
+ * Problem URL: https://atcoder.jp/contests/rcl-contest-2021-long/tasks/rcl_contest_2021_long_a
+ * Result: AC
+ * Execution Time: 20 ms
+ */
+
+for i in {1..1000};do echo -1;done
