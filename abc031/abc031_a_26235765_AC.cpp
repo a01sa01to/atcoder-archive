fe856d6d556056fd448d3e7e83c67759fe530e13
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-30 14:26:47 +0900

    [AC] SubmissionID: 26235765 at 2021-09-30 14:26:47 (1632979607)

---
+++ abc031/abc031_a_26235765_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc031/submissions/26235765
+ * Submitted at: 2021-09-30 14:26:47
+ * Problem URL: https://atcoder.jp/contests/abc031/tasks/abc031_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int a,d;
+  cin>>a>>d;
+  cout<<a*d+max(a,d)<<endl;
+  return 0;
+}
