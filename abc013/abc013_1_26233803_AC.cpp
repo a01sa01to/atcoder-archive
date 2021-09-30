ec57ba68a084067dc716d37ca095ee1d9883b9e1
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-30 11:34:36 +0900

    [AC] SubmissionID: 26233803 at 2021-09-30 11:34:36 (1632969276)

---
+++ abc013/abc013_1_26233803_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc013/submissions/26233803
+ * Submitted at: 2021-09-30 11:34:36
+ * Problem URL: https://atcoder.jp/contests/abc013/tasks/abc013_1
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  char x;
+  cin >> x;
+  cout << x-'A'+1 << endl;
+  return 0;
+}
