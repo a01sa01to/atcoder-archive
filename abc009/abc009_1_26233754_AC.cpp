fb138e996f7be104184591b99e132db03e503065
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-30 11:30:00 +0900

    [AC] SubmissionID: 26233754 at 2021-09-30 11:30:00 (1632969000)

---
+++ abc009/abc009_1_26233754_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc009/submissions/26233754
+ * Submitted at: 2021-09-30 11:30:00
+ * Problem URL: https://atcoder.jp/contests/abc009/tasks/abc009_1
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int x;
+  cin >> x;
+  cout << (x+1)/2 << endl;
+  return 0;
+}
