02f1a220407ce52ce29566b2e1243214be74af01
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-30 11:28:06 +0900

    [AC] SubmissionID: 26233732 at 2021-09-30 11:28:06 (1632968886)

---
+++ abc006/abc006_1_26233732_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc006/submissions/26233732
+ * Submitted at: 2021-09-30 11:28:06
+ * Problem URL: https://atcoder.jp/contests/abc006/tasks/abc006_1
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int x;
+  cin >> x;
+  cout << (x%3 ? "NO" : "YES") << endl;
+  return 0;
+}
