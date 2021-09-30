7b82aa08a8429e15a58742e92ac5453a20ebc0d7
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-30 11:47:45 +0900

    [AC] SubmissionID: 26233926 at 2021-09-30 11:47:45 (1632970065)

---
+++ abc020/abc020_a_26233926_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc020/submissions/26233926
+ * Submitted at: 2021-09-30 11:47:45
+ * Problem URL: https://atcoder.jp/contests/abc020/tasks/abc020_a
+ * Result: AC
+ * Execution Time: 4 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int x;
+  cin>>x;
+  cout<<(--x?"chokudai":"ABC")<<endl;
+  return 0;
+}
