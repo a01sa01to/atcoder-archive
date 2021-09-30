37934e8a4641b9de8460823eedf62cb8d0b9960e
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-30 12:12:31 +0900

    [AC] SubmissionID: 26234198 at 2021-09-30 12:12:31 (1632971551)

---
+++ abc026/abc026_a_26234198_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc026/submissions/26234198
+ * Submitted at: 2021-09-30 12:12:31
+ * Problem URL: https://atcoder.jp/contests/abc026/tasks/abc026_a
+ * Result: AC
+ * Execution Time: 4 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int x;
+  cin>>x;
+  cout<<x*x/4<<endl;
+  return 0;
+}
