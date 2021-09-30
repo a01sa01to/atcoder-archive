706d2ad1e8aeeada22c3691256326285754ec9fc
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-30 11:39:54 +0900

    [AC] SubmissionID: 26233853 at 2021-09-30 11:39:54 (1632969594)

---
+++ abc017/abc017_1_26233853_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc017/submissions/26233853
+ * Submitted at: 2021-09-30 11:39:54
+ * Problem URL: https://atcoder.jp/contests/abc017/tasks/abc017_1
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int a,b,c,d,e,f;
+  cin>>a>>b>>c>>d>>e>>f;
+  cout << (a*b+c*d+e*f)/10 << endl;
+  return 0;
+}
