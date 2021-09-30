0ef4628df41118f07766d2087691d0493e5cc684
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-30 11:26:42 +0900

    [AC] SubmissionID: 26233716 at 2021-09-30 11:26:42 (1632968802)

---
+++ abc003/abc003_1_26233716_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc003/submissions/26233716
+ * Submitted at: 2021-09-30 11:26:42
+ * Problem URL: https://atcoder.jp/contests/abc003/tasks/abc003_1
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int x;
+  cin >> x;
+  cout << (x+1)*5000 << endl;
+  return 0;
+}
