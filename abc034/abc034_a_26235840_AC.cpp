8a7c80fae899c827f42817a87b5fe90af1ad2a0a
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-30 14:34:24 +0900

    [AC] SubmissionID: 26235840 at 2021-09-30 14:34:24 (1632980064)

---
+++ abc034/abc034_a_26235840_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc034/submissions/26235840
+ * Submitted at: 2021-09-30 14:34:24
+ * Problem URL: https://atcoder.jp/contests/abc034/tasks/abc034_a
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int x,y;
+  cin>>x>>y;
+  cout<<(x<y?"Better":"Worse")<<endl;
+  return 0;
+}
