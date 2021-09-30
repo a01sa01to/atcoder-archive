5ab9b3698750e238edaaf8f200a0a68719162fdb
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-30 11:29:26 +0900

    [AC] SubmissionID: 26233747 at 2021-09-30 11:29:26 (1632968966)

---
+++ abc008/abc008_1_26233747_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc008/submissions/26233747
+ * Submitted at: 2021-09-30 11:29:26
+ * Problem URL: https://atcoder.jp/contests/abc008/tasks/abc008_1
+ * Result: AC
+ * Execution Time: 10 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int s,t;
+  cin >> s >> t;
+  cout << t-s+1 << endl;
+  return 0;
+}
