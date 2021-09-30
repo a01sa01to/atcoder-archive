feccad66939e2f364a4b6016ef472b3cf6000dc2
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-30 11:27:37 +0900

    [AC] SubmissionID: 26233727 at 2021-09-30 11:27:37 (1632968857)

---
+++ abc005/abc005_1_26233727_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc005/submissions/26233727
+ * Submitted at: 2021-09-30 11:27:37
+ * Problem URL: https://atcoder.jp/contests/abc005/tasks/abc005_1
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int x, y;
+  cin >> x >> y;
+  cout << y/x << endl;
+  return 0;
+}
