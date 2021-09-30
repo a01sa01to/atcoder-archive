566e426f3f81c3948b25bf390e2ddb275b023947
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-30 11:23:52 +0900

    [AC] SubmissionID: 26233687 at 2021-09-30 11:23:52 (1632968632)

---
+++ abc001/abc001_1_26233687_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc001/submissions/26233687
+ * Submitted at: 2021-09-30 11:23:52
+ * Problem URL: https://atcoder.jp/contests/abc001/tasks/abc001_1
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int h1, h2;
+  cin >> h1 >> h2;
+  cout << h1 - h2 << endl;
+  return 0;
+}
