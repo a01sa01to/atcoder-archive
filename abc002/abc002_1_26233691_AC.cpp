4c0b61b2d3133316383af52950eeb959eb3326fb
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-30 11:24:29 +0900

    [AC] SubmissionID: 26233691 at 2021-09-30 11:24:29 (1632968669)

---
+++ abc002/abc002_1_26233691_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc002/submissions/26233691
+ * Submitted at: 2021-09-30 11:24:29
+ * Problem URL: https://atcoder.jp/contests/abc002/tasks/abc002_1
+ * Result: AC
+ * Execution Time: 10 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int x, y;
+  cin >> x >> y;
+  cout << max(x,y) << endl;
+  return 0;
+}
