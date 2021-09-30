886b7900317facaf8804ca1fe72c3c5fdfc5f673
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-30 11:34:03 +0900

    [AC] SubmissionID: 26233797 at 2021-09-30 11:34:03 (1632969243)

---
+++ abc012/abc012_1_26233797_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc012/submissions/26233797
+ * Submitted at: 2021-09-30 11:34:03
+ * Problem URL: https://atcoder.jp/contests/abc012/tasks/abc012_1
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int x,y;
+  cin >> x >> y;
+  printf("%d %d\n",y,x);
+  return 0;
+}
