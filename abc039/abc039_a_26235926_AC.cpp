b118f93344f9b526c539262329037ebe595967b3
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-30 14:43:19 +0900

    [AC] SubmissionID: 26235926 at 2021-09-30 14:43:19 (1632980599)

---
+++ abc039/abc039_a_26235926_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc039/submissions/26235926
+ * Submitted at: 2021-09-30 14:43:19
+ * Problem URL: https://atcoder.jp/contests/abc039/tasks/abc039_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int x,y,z;
+  cin>>x>>y>>z;
+  cout<<(x*y+y*z+z*x)*2<<endl;
+  return 0;
+}
