345c901df03af65cc77e141a13ca19a7079b3a2a
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-30 14:42:31 +0900

    [AC] SubmissionID: 26235915 at 2021-09-30 14:42:31 (1632980551)

---
+++ abc038/abc038_a_26235915_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc038/submissions/26235915
+ * Submitted at: 2021-09-30 14:42:31
+ * Problem URL: https://atcoder.jp/contests/abc038/tasks/abc038_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  string x;
+  cin>>x;
+  cout<<(x.back()=='T'?"YES":"NO")<<endl;
+  return 0;
+}
