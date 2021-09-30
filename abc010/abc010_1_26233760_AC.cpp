32eaf363d5ba9741474da592665f5af7db2fc39b
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-30 11:30:30 +0900

    [AC] SubmissionID: 26233760 at 2021-09-30 11:30:30 (1632969030)

---
+++ abc010/abc010_1_26233760_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc010/submissions/26233760
+ * Submitted at: 2021-09-30 11:30:30
+ * Problem URL: https://atcoder.jp/contests/abc010/tasks/abc010_1
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  string x;
+  cin >> x;
+  cout << x << "pp" << endl;
+  return 0;
+}
