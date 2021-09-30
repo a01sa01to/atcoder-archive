f0113df63c35d7e7a5b8320b36877d65ec3596f4
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-30 11:50:50 +0900

    [AC] SubmissionID: 26233947 at 2021-09-30 11:50:50 (1632970250)

---
+++ abc021/abc021_a_26233947_AC.cpp
@@ -0,0 +1,18 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc021/submissions/26233947
+ * Submitted at: 2021-09-30 11:50:50
+ * Problem URL: https://atcoder.jp/contests/abc021/tasks/abc021_a
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int x;
+  cin>>x;
+  cout<<x<<endl;
+  while(x--)cout<<1<<endl;
+  return 0;
+}
