fcb9172bd83133db897244690cdb74f4fac04158
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-30 11:27:05 +0900

    [AC] SubmissionID: 26233721 at 2021-09-30 11:27:05 (1632968825)

---
+++ abc004/abc004_1_26233721_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc004/submissions/26233721
+ * Submitted at: 2021-09-30 11:27:05
+ * Problem URL: https://atcoder.jp/contests/abc004/tasks/abc004_1
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int x;
+  cin >> x;
+  cout << 2*x << endl;
+  return 0;
+}
