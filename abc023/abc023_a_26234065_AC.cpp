789c93de22861131a63732d449d1be000a29f31e
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-30 12:01:14 +0900

    [AC] SubmissionID: 26234065 at 2021-09-30 12:01:14 (1632970874)

---
+++ abc023/abc023_a_26234065_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc023/submissions/26234065
+ * Submitted at: 2021-09-30 12:01:14
+ * Problem URL: https://atcoder.jp/contests/abc023/tasks/abc023_a
+ * Result: AC
+ * Execution Time: 3 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int x;
+  cin>>x;
+  cout<<x/10+x%10<<endl;
+  return 0;
+}
