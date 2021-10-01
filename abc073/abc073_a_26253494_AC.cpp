640ce5ae4bea8fe26b7c0be3e32d9b81dbadbb16
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 19:21:36 +0900

    [AC] SubmissionID: 26253494 at 2021-10-01 19:21:36 (1633083696)

---
+++ abc073/abc073_a_26253494_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc073/submissions/26253494
+ * Submitted at: 2021-10-01 19:21:36
+ * Problem URL: https://atcoder.jp/contests/abc073/tasks/abc073_a
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  string s;
+  cin>>s;
+  cout<<(s[1]=='9'||s[0]=='9'?"Yes":"No")<<endl;
+}
