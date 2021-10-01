5687f74ee524775664781715df84a1dde48aa9e7
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 23:27:11 +0900

    [AC] SubmissionID: 26257457 at 2021-10-01 23:27:11 (1633098431)

---
+++ abc133/abc133_a_26257457_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc133/submissions/26257457
+ * Submitted at: 2021-10-01 23:27:11
+ * Problem URL: https://atcoder.jp/contests/abc133/tasks/abc133_a
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include<bits/stdc++.h>
+main(){int a,b,c;std::cin>>a>>b>>c;std::cout<<std::min(a*b,c);}
