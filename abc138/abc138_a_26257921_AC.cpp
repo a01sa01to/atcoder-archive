7dedad68be1f3edd702fb76bbeb172d47c5fca54
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 23:54:37 +0900

    [AC] SubmissionID: 26257921 at 2021-10-01 23:54:37 (1633100077)

---
+++ abc138/abc138_a_26257921_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc138/submissions/26257921
+ * Submitted at: 2021-10-01 23:54:37
+ * Problem URL: https://atcoder.jp/contests/abc138/tasks/abc138_a
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include<bits/stdc++.h>
+main(){std::string s;int a;std::cin>>a>>s;std::cout<<(a<3200?"red":s);}
