12538e438644a2fd7410517101dc6406d5abccd2
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 18:52:23 +0900

    [AC] SubmissionID: 26253161 at 2021-10-01 18:52:23 (1633081943)

---
+++ abc064/abc064_a_26253161_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc064/submissions/26253161
+ * Submitted at: 2021-10-01 18:52:23
+ * Problem URL: https://atcoder.jp/contests/abc064/tasks/abc064_a
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int a,b,c;
+  cin>>a>>b>>c;
+  cout<<((10*b+c)%4?"NO":"YES")<<endl;
+}
