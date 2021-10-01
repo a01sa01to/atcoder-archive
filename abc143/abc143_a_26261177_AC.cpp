74769ea51a3c0c76748a6da10fa96f5ba9d7d272
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-02 08:50:29 +0900

    [AC] SubmissionID: 26261177 at 2021-10-02 08:50:29 (1633132229)

---
+++ abc143/abc143_a_26261177_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc143/submissions/26261177
+ * Submitted at: 2021-10-02 08:50:29
+ * Problem URL: https://atcoder.jp/contests/abc143/tasks/abc143_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<iostream>
+main(){int a,b;std::cin>>a>>b;std::cout<<std::max(0,a-b-b);}
