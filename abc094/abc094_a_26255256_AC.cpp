00ae3dad1b8ef1e5df450ec7a181ba107ef85bb2
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 21:23:40 +0900

    [AC] SubmissionID: 26255256 at 2021-10-01 21:23:40 (1633091020)

---
+++ abc094/abc094_a_26255256_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc094/submissions/26255256
+ * Submitted at: 2021-10-01 21:23:40
+ * Problem URL: https://atcoder.jp/contests/abc094/tasks/abc094_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int a,b,c;
+  cin>>a>>b>>c;
+  if(a<=c&&c<=a+b) cout<<"YES"<<endl;
+  else cout<<"NO"<<endl;
+}
