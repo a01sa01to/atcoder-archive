e8c5f96a2649faa22ae2de9231bea74ad53eef0d
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 22:05:51 +0900

    [AC] SubmissionID: 26256123 at 2021-10-01 22:05:51 (1633093551)

---
+++ abc105/abc105_a_26256123_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc105/submissions/26256123
+ * Submitted at: 2021-10-01 22:05:51
+ * Problem URL: https://atcoder.jp/contests/abc105/tasks/abc105_a
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include<iostream>
+main(){int a,b;std::cin>>a>>b;std::cout<<!!(a%b);}
