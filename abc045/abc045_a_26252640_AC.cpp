81b088698f35c0d5ccfcb4bc3cc9d0e88e3e8c0d
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 18:12:32 +0900

    [AC] SubmissionID: 26252640 at 2021-10-01 18:12:32 (1633079552)

---
+++ abc045/abc045_a_26252640_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc045/submissions/26252640
+ * Submitted at: 2021-10-01 18:12:32
+ * Problem URL: https://atcoder.jp/contests/abc045/tasks/abc045_a
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int a,b,c;
+  cin>>a>>b>>c;
+  cout<<(a+b)*c/2<<endl;
+}
