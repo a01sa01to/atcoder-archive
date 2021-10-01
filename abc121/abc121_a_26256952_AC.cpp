4c958d1516ca507deb1fd98d1d9e9615d2554d1d
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 22:52:51 +0900

    [AC] SubmissionID: 26256952 at 2021-10-01 22:52:51 (1633096371)

---
+++ abc121/abc121_a_26256952_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc121/submissions/26256952
+ * Submitted at: 2021-10-01 22:52:51
+ * Problem URL: https://atcoder.jp/contests/abc121/tasks/abc121_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include<iostream>
+main(){int a,b,c,d;std::cin>>a>>b>>c>>d;std::cout<<(a-c)*(b-d);}
