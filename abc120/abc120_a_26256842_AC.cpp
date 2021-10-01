8e41947c4a6bfbb60b39d755e9632ee236fd4d7b
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 22:47:48 +0900

    [AC] SubmissionID: 26256842 at 2021-10-01 22:47:48 (1633096068)

---
+++ abc120/abc120_a_26256842_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc120/submissions/26256842
+ * Submitted at: 2021-10-01 22:47:48
+ * Problem URL: https://atcoder.jp/contests/abc120/tasks/abc120_a
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;main(){int a,b,c;cin>>a>>b>>c;cout<<min(b/a,c);}
