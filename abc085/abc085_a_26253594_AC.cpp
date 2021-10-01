6b89a64369c8a94971134e559ed6937147b7cfb4
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 19:28:41 +0900

    [AC] SubmissionID: 26253594 at 2021-10-01 19:28:41 (1633084121)

---
+++ abc085/abc085_a_26253594_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc085/submissions/26253594
+ * Submitted at: 2021-10-01 19:28:41
+ * Problem URL: https://atcoder.jp/contests/abc085/tasks/abc085_a
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;main(){string s;cin>>s;s[3]='8';cout<<s<<endl;}
