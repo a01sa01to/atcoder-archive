6c30ff78e67c6f8519319a435b41399f41551bbd
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 22:50:45 +0900

    [AC] SubmissionID: 26256904 at 2021-10-01 22:50:45 (1633096245)

---
+++ abc121/abc121_a_26256904_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc121/submissions/26256904
+ * Submitted at: 2021-10-01 22:50:45
+ * Problem URL: https://atcoder.jp/contests/abc121/tasks/abc121_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<bits/stdc++.h>
+main(){int a,b,c,d;std::cin>>a>>b>>c>>d;std::cout<<(a-c)*(b-d);}
