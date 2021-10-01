33ee15aee4268f5dc3a86cd21e606f40a731d835
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-02 08:51:55 +0900

    [AC] SubmissionID: 26261183 at 2021-10-02 08:51:55 (1633132315)

---
+++ abc144/abc144_a_26261183_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc144/submissions/26261183
+ * Submitted at: 2021-10-02 08:51:55
+ * Problem URL: https://atcoder.jp/contests/abc144/tasks/abc144_a
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include<iostream>
+main(){int a,b;std::cin>>a>>b;std::cout<<(a>9||b>9?-1:a*b);}
