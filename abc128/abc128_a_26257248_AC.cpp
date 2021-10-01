8febcc3c335f3f1e91338530ac6ca753649df178
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 23:13:34 +0900

    [AC] SubmissionID: 26257248 at 2021-10-01 23:13:34 (1633097614)

---
+++ abc128/abc128_a_26257248_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc128/submissions/26257248
+ * Submitted at: 2021-10-01 23:13:34
+ * Problem URL: https://atcoder.jp/contests/abc128/tasks/abc128_a
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include<iostream>
+main(){int a,b;std::cin>>a>>b;std::cout<<(a*3+b)/2;}
