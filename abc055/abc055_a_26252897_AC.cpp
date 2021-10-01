21ef773ce991f56a6fe7d688d912affda7ece68a
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 18:31:45 +0900

    [AC] SubmissionID: 26252897 at 2021-10-01 18:31:45 (1633080705)

---
+++ abc055/abc055_a_26252897_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc055/submissions/26252897
+ * Submitted at: 2021-10-01 18:31:45
+ * Problem URL: https://atcoder.jp/contests/abc055/tasks/abc055_a
+ * Result: AC
+ * Execution Time: 3 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int x;cin>>x;
+  cout<<800*x-200*(x/15)<<endl;
+}
