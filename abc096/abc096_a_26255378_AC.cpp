97c8eb357432a5d90123b6278acfd85b5b50a74f
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 21:29:44 +0900

    [AC] SubmissionID: 26255378 at 2021-10-01 21:29:44 (1633091384)

---
+++ abc096/abc096_a_26255378_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc096/submissions/26255378
+ * Submitted at: 2021-10-01 21:29:44
+ * Problem URL: https://atcoder.jp/contests/abc096/tasks/abc096_a
+ * Result: AC
+ * Execution Time: 10 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int a,b;
+  cin>>a>>b;
+  if(a>b) cout<<--a<<endl;
+  else cout<<a<<endl;
+}
