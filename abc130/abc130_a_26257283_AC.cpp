330746bd75c51ba4daab54d3aeec166ebb1e6465
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 23:16:05 +0900

    [AC] SubmissionID: 26257283 at 2021-10-01 23:16:05 (1633097765)

---
+++ abc130/abc130_a_26257283_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc130/submissions/26257283
+ * Submitted at: 2021-10-01 23:16:05
+ * Problem URL: https://atcoder.jp/contests/abc130/tasks/abc130_a
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+#include<iostream>
+main(){int a,b;std::cin>>a>>b;puts(a<b?"0":"10");}
