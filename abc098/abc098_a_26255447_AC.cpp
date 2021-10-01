f650d91d046e64e6c2fe2f402065c924811487c6
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 21:32:47 +0900

    [AC] SubmissionID: 26255447 at 2021-10-01 21:32:47 (1633091567)

---
+++ abc098/abc098_a_26255447_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc098/submissions/26255447
+ * Submitted at: 2021-10-01 21:32:47
+ * Problem URL: https://atcoder.jp/contests/abc098/tasks/abc098_a
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;main(){int a,b;cin>>a>>b;cout<<max({a+b,a-b,a*b});}
