3cf8c11f926c28ebbec15c789cb00d2e2b94fd9f
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 22:02:45 +0900

    [AC] SubmissionID: 26256081 at 2021-10-01 22:02:45 (1633093365)

---
+++ abc103/abc103_a_26256081_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc103/submissions/26256081
+ * Submitted at: 2021-10-01 22:02:45
+ * Problem URL: https://atcoder.jp/contests/abc103/tasks/abc103_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;main(){int a,b,c;cin>>a>>b>>c;cout<<max({a,b,c})-min({a,b,c})<<endl;}
