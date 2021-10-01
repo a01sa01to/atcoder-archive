4e6677c16c768105ead73b4620acd8d398ce631f
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 19:27:18 +0900

    [AC] SubmissionID: 26253578 at 2021-10-01 19:27:18 (1633084038)

---
+++ abc078/abc078_a_26253578_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc078/submissions/26253578
+ * Submitted at: 2021-10-01 19:27:18
+ * Problem URL: https://atcoder.jp/contests/abc078/tasks/abc078_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<iostream>
+main(){char a,b;std::cin>>a>>b;std::cout<<(a>b?">":(a<b?"<":"="))<<std::endl;}
