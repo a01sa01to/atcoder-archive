b0130cbc3d7312eb9cbacc338fa9f33ad52c9133
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 18:40:39 +0900

    [AC] SubmissionID: 26253007 at 2021-10-01 18:40:39 (1633081239)

---
+++ abc059/abc059_a_26253007_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc059/submissions/26253007
+ * Submitted at: 2021-10-01 18:40:39
+ * Problem URL: https://atcoder.jp/contests/abc059/tasks/abc059_a
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  string a,b,c;
+  cin>>a>>b>>c;
+  cout<<string({toupper(a[0]),toupper(b[0]),toupper(c[0])})<<endl;
+}
