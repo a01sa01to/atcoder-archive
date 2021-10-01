e52a9feab33db36a760046ee728f8b49eb593388
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 21:34:57 +0900

    [AC] SubmissionID: 26255505 at 2021-10-01 21:34:57 (1633091697)

---
+++ abc099/abc099_a_26255505_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc099/submissions/26255505
+ * Submitted at: 2021-10-01 21:34:57
+ * Problem URL: https://atcoder.jp/contests/abc099/tasks/abc099_a
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int x;cin>>x;
+  if(x>999) cout<<"ABD"<<endl;
+  else cout<<"ABC"<<endl;
+}
