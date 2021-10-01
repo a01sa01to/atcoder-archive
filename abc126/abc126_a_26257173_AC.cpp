76a9a03195825f529c2e0105f51818828d22ba16
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 23:07:59 +0900

    [AC] SubmissionID: 26257173 at 2021-10-01 23:07:59 (1633097279)

---
+++ abc126/abc126_a_26257173_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc126/submissions/26257173
+ * Submitted at: 2021-10-01 23:07:59
+ * Problem URL: https://atcoder.jp/contests/abc126/tasks/abc126_a
+ * Result: AC
+ * Execution Time: 11 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;main(){string s;int k;cin>>k>>k>>s;s[--k]+='a'-'A';cout<<s;}
