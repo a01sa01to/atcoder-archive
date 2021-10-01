fc498ed3271881fefee0341eee5220210ec725ab
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 22:04:08 +0900

    [AC] SubmissionID: 26256100 at 2021-10-01 22:04:08 (1633093448)

---
+++ abc104/abc104_a_26256100_AC.cpp
@@ -0,0 +1,18 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc104/submissions/26256100
+ * Submitted at: 2021-10-01 22:04:08
+ * Problem URL: https://atcoder.jp/contests/abc104/tasks/abc104_a
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int r;
+  cin>>r;
+  if(r<1200) cout<<"ABC"<<endl;
+  else if(r<2800) cout<<"ARC"<<endl;
+  else cout<<"AGC"<<endl;
+}
