6ff09be78d6e0d5adb2c771f63f5399d71d5d1f7
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 19:43:57 +0900

    [AC] SubmissionID: 26253761 at 2021-10-01 19:43:57 (1633085037)

---
+++ abc092/abc092_a_26253761_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc092/submissions/26253761
+ * Submitted at: 2021-10-01 19:43:57
+ * Problem URL: https://atcoder.jp/contests/abc092/tasks/abc092_a
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;main(){int a,b,c,d;cin>>a>>b>>c>>d;cout<<min(a,b)+min(c,d)<<endl;}
