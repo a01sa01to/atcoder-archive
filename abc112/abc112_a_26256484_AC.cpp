ceb48175821fe18a48fb4e2f0d24a691563b8866
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 22:27:40 +0900

    [AC] SubmissionID: 26256484 at 2021-10-01 22:27:40 (1633094860)

---
+++ abc112/abc112_a_26256484_AC.cpp
@@ -0,0 +1,18 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc112/submissions/26256484
+ * Submitted at: 2021-10-01 22:27:40
+ * Problem URL: https://atcoder.jp/contests/abc112/tasks/abc112_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int a;cin>>a;
+  if(a==1) cout<<"Hello World"<<endl;
+  else{
+    int b,c;cin>>b>>c;cout<<b+c<<endl;
+  }
+}
