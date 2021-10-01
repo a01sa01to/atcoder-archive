9ae87499691599e25a48afa25d67cf6a5f736dd6
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 22:37:53 +0900

    [AC] SubmissionID: 26256658 at 2021-10-01 22:37:53 (1633095473)

---
+++ abc115/abc115_a_26256658_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc115/submissions/26256658
+ * Submitted at: 2021-10-01 22:37:53
+ * Problem URL: https://atcoder.jp/contests/abc115/tasks/abc115_a
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include<bits/stdc++.h>
+main(){int t;std::cin>>t;t-=25;printf("Christmas");while(t++)printf(" Eve");}
