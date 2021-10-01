6fa86d69f9495e938b94758ea15ddc142a45666f
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 22:32:04 +0900

    [AC] SubmissionID: 26256572 at 2021-10-01 22:32:04 (1633095124)

---
+++ abc114/abc114_a_26256572_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc114/submissions/26256572
+ * Submitted at: 2021-10-01 22:32:04
+ * Problem URL: https://atcoder.jp/contests/abc114/tasks/abc114_a
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int a;cin>>a;
+  cout<<(a==7||a==5||a==3?"YES":"NO")<<endl;
+}
