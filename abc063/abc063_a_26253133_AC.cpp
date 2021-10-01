1975e1e8292e3b3ae8dcfec4f8917014bd8d8f21
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 18:50:29 +0900

    [AC] SubmissionID: 26253133 at 2021-10-01 18:50:29 (1633081829)

---
+++ abc063/abc063_a_26253133_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc063/submissions/26253133
+ * Submitted at: 2021-10-01 18:50:29
+ * Problem URL: https://atcoder.jp/contests/abc063/tasks/abc063_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int a,b;
+  cin>>a>>b;
+  cout<<(a+b<10?to_string(a+b):"error")<<endl;
+}
