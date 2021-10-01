44e93e0d68bbc921981f7aa5b6f43b5061d3f5bf
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 22:12:24 +0900

    [AC] SubmissionID: 26256228 at 2021-10-01 22:12:24 (1633093944)

---
+++ abc108/abc108_a_26256228_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc108/submissions/26256228
+ * Submitted at: 2021-10-01 22:12:24
+ * Problem URL: https://atcoder.jp/contests/abc108/tasks/abc108_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<iostream>
+main(){int n;std::cin>>n;std::cout<<n/2*(n-n/2);}
