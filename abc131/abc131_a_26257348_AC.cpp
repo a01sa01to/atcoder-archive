b9eb70e721cd40c190695cd111af3356546953c7
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 23:20:30 +0900

    [AC] SubmissionID: 26257348 at 2021-10-01 23:20:30 (1633098030)

---
+++ abc131/abc131_a_26257348_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc131/submissions/26257348
+ * Submitted at: 2021-10-01 23:20:30
+ * Problem URL: https://atcoder.jp/contests/abc131/tasks/abc131_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;main(){string s;cin>>s;puts(s[0]==s[1]||s[1]==s[2]||s[2]==s[3]?"Bad":"Good");}
