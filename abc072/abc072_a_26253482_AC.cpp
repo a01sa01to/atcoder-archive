2d955283cd91f413a65d3cf73cdf0fffe1e0aa01
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 19:20:40 +0900

    [AC] SubmissionID: 26253482 at 2021-10-01 19:20:40 (1633083640)

---
+++ abc072/abc072_a_26253482_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc072/submissions/26253482
+ * Submitted at: 2021-10-01 19:20:40
+ * Problem URL: https://atcoder.jp/contests/abc072/tasks/abc072_a
+ * Result: AC
+ * Execution Time: 11 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;main(){int a,b;cin>>a>>b;cout<<max(0,a-b)<<endl;}
