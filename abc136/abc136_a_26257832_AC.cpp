e91dc7871459ef438ce84a6405d051657296f5b6
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 23:50:09 +0900

    [AC] SubmissionID: 26257832 at 2021-10-01 23:50:09 (1633099809)

---
+++ abc136/abc136_a_26257832_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc136/submissions/26257832
+ * Submitted at: 2021-10-01 23:50:09
+ * Problem URL: https://atcoder.jp/contests/abc136/tasks/abc136_a
+ * Result: AC
+ * Execution Time: 12 ms
+ */
+
+#include<iostream>
+main(){int a,b,c;std::cin>>a>>b>>c;std::cout<<std::max(0,c-a+b);}
