e63d6b8b9fa9d3b67c2f234577320792000a2dc4
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-02 08:56:46 +0900

    [AC] SubmissionID: 26261202 at 2021-10-02 08:56:46 (1633132606)

---
+++ abc147/abc147_a_26261202_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc147/submissions/26261202
+ * Submitted at: 2021-10-02 08:56:46
+ * Problem URL: https://atcoder.jp/contests/abc147/tasks/abc147_a
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include<iostream>
+main(){int a,b,c;std::cin>>a>>b>>c;puts(a+b+c>21?"bust":"win");}
