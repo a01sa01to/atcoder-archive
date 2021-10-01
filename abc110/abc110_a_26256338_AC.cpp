4ea0ac1eb9dcb34d2999aa3615e2a9567eb22b74
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 22:18:52 +0900

    [AC] SubmissionID: 26256338 at 2021-10-01 22:18:52 (1633094332)

---
+++ abc110/abc110_a_26256338_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc110/submissions/26256338
+ * Submitted at: 2021-10-01 22:18:52
+ * Problem URL: https://atcoder.jp/contests/abc110/tasks/abc110_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;int main(){int a,b,c;cin>>a>>b>>c;cout<<max({a,b,c})*9+a+b+c;}
