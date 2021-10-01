be137004419c32dd2750544b5b3613371f2e4b39
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 19:36:17 +0900

    [AC] SubmissionID: 26253687 at 2021-10-01 19:36:17 (1633084577)

---
+++ abc090/abc090_a_26253687_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc090/submissions/26253687
+ * Submitted at: 2021-10-01 19:36:17
+ * Problem URL: https://atcoder.jp/contests/abc090/tasks/abc090_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;main(){string a,b,c;cin>>a>>b>>c;cout<<a[0]<<b[1]<<c[2]<<endl;}
