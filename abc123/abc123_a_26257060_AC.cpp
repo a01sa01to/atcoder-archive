f4b77769a5536e60dcde87ecf30ecb1cbdbf6a91
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 22:59:23 +0900

    [AC] SubmissionID: 26257060 at 2021-10-01 22:59:23 (1633096763)

---
+++ abc123/abc123_a_26257060_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc123/submissions/26257060
+ * Submitted at: 2021-10-01 22:59:23
+ * Problem URL: https://atcoder.jp/contests/abc123/tasks/abc123_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include<iostream>
+main(){int a,b,c,d,e,f;std::cin>>a>>b>>c>>d>>e>>f;std::cout<<(e-a>f?":(":"Yay!");}
