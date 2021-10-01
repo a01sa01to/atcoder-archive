1a658e8894269962932e341e7ccc10a589a954de
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 19:03:51 +0900

    [AC] SubmissionID: 26253301 at 2021-10-01 19:03:51 (1633082631)

---
+++ abc070/abc070_a_26253301_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc070/submissions/26253301
+ * Submitted at: 2021-10-01 19:03:51
+ * Problem URL: https://atcoder.jp/contests/abc070/tasks/abc070_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  string s;cin>>s;
+  cout<<(s[0]==s[2]?"Yes":"No")<<endl;
+}
