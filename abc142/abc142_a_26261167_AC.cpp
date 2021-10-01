7abb1d9f1502e73d44b4e5780856f7e2fc01377c
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-02 08:48:27 +0900

    [AC] SubmissionID: 26261167 at 2021-10-02 08:48:27 (1633132107)

---
+++ abc142/abc142_a_26261167_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc142/submissions/26261167
+ * Submitted at: 2021-10-02 08:48:27
+ * Problem URL: https://atcoder.jp/contests/abc142/tasks/abc142_a
+ * Result: AC
+ * Execution Time: 10 ms
+ */
+
+#include<bits/stdc++.h>
+main(){int a;std::cin>>a;std::cout<<0.5+(a%2)*0.5/a;}
