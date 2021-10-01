9d155768fbf50a98b45fbc6cdae092440ebd76d4
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 18:28:24 +0900

    [AC] SubmissionID: 26252848 at 2021-10-01 18:28:24 (1633080504)

---
+++ abc053/abc053_a_26252848_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc053/submissions/26252848
+ * Submitted at: 2021-10-01 18:28:24
+ * Problem URL: https://atcoder.jp/contests/abc053/tasks/abc053_a
+ * Result: AC
+ * Execution Time: 3 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int x; cin>>x;
+  cout<<(x<1200?"ABC":"ARC")<<endl;
+}
