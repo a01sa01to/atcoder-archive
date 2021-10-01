9d6707ecadb9e75d62b1a648f4aa86bd3581e829
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 21:37:04 +0900

    [AC] SubmissionID: 26255538 at 2021-10-01 21:37:04 (1633091824)

---
+++ abc100/abc100_a_26255538_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc100/submissions/26255538
+ * Submitted at: 2021-10-01 21:37:04
+ * Problem URL: https://atcoder.jp/contests/abc100/tasks/abc100_a
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int a,b;
+  cin>>a>>b;
+  if(a>8||b>8)cout<<":("<<endl;
+  else cout<<"Yay!"<<endl;
+}
