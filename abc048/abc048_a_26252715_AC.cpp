f7c5209bb05e0161dc6e2a86b15f7babd621ceb0
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 18:18:18 +0900

    [AC] SubmissionID: 26252715 at 2021-10-01 18:18:18 (1633079898)

---
+++ abc048/abc048_a_26252715_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc048/submissions/26252715
+ * Submitted at: 2021-10-01 18:18:18
+ * Problem URL: https://atcoder.jp/contests/abc048/tasks/abc048_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  string s;
+  cin>>s>>s;
+  printf("A%cC",s[0]);
+}
