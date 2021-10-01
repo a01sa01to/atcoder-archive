85195c648421f9fb3b55175e76d2a0ed0fcd7468
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-02 08:40:48 +0900

    [AC] SubmissionID: 26261128 at 2021-10-02 08:40:48 (1633131648)

---
+++ abc141/abc141_a_26261128_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc141/submissions/26261128
+ * Submitted at: 2021-10-02 08:40:48
+ * Problem URL: https://atcoder.jp/contests/abc141/tasks/abc141_a
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  string s;cin>>s;
+  if(s=="Sunny")cout<<"Cloudy"<<endl;
+  if(s=="Cloudy")cout<<"Rainy"<<endl;
+  if(s=="Rainy")cout<<"Sunny"<<endl;
+}
