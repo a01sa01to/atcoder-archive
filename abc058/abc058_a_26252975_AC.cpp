3566ae9915a39126250937d331ae79e9d5ed8866
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 18:37:23 +0900

    [AC] SubmissionID: 26252975 at 2021-10-01 18:37:23 (1633081043)

---
+++ abc058/abc058_a_26252975_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc058/submissions/26252975
+ * Submitted at: 2021-10-01 18:37:23
+ * Problem URL: https://atcoder.jp/contests/abc058/tasks/abc058_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;main(){int a,b,c;cin>>a>>b>>c;cout<<(b-a==c-b?"YES":"NO")<<endl;}
