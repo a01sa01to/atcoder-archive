4eb20b6ea929a4d4669177e0bbb68a4d16961a4d
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 18:33:44 +0900

    [AC] SubmissionID: 26252923 at 2021-10-01 18:33:44 (1633080824)

---
+++ abc056/abc056_a_26252923_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc056/submissions/26252923
+ * Submitted at: 2021-10-01 18:33:44
+ * Problem URL: https://atcoder.jp/contests/abc056/tasks/abc056_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  char a,b; cin>>a>>b;
+  if(a==b) cout<<"H"<<endl;
+  else cout<<"D"<<endl;
+}
