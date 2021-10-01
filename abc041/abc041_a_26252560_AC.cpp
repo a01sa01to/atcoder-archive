637ba5e5440bad76dd8e3d13d8990d33aa603853
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 18:06:15 +0900

    [AC] SubmissionID: 26252560 at 2021-10-01 18:06:15 (1633079175)

---
+++ abc041/abc041_a_26252560_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc041/submissions/26252560
+ * Submitted at: 2021-10-01 18:06:15
+ * Problem URL: https://atcoder.jp/contests/abc041/tasks/abc041_a
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  string s; int i;
+  cin>>s>>i;
+  cout<<s[--i]<<endl;
+}
