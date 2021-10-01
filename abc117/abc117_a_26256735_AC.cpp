a6a50648e3442246437bb061b0aed18ce2b18602
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 22:42:18 +0900

    [AC] SubmissionID: 26256735 at 2021-10-01 22:42:18 (1633095738)

---
+++ abc117/abc117_a_26256735_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc117/submissions/26256735
+ * Submitted at: 2021-10-01 22:42:18
+ * Problem URL: https://atcoder.jp/contests/abc117/tasks/abc117_a
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+#include<iostream>
+main(){float t,x;std::cin>>t>>x;std::cout<<t/x;}
