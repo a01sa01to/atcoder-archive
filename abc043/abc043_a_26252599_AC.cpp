61b96b577c95f8cea1f887f6efcffcd096a173c4
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 18:09:35 +0900

    [AC] SubmissionID: 26252599 at 2021-10-01 18:09:35 (1633079375)

---
+++ abc043/abc043_a_26252599_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc043/submissions/26252599
+ * Submitted at: 2021-10-01 18:09:35
+ * Problem URL: https://atcoder.jp/contests/abc043/tasks/abc043_a
+ * Result: AC
+ * Execution Time: 4 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int a;
+  cin>>a;
+  cout<<a*(a+1)/2<<endl;
+}
