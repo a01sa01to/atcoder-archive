18311573a9c606fa89005a1952501c263194e9ba
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 22:45:54 +0900

    [AC] SubmissionID: 26256806 at 2021-10-01 22:45:54 (1633095954)

---
+++ abc119/abc119_a_26256806_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc119/submissions/26256806
+ * Submitted at: 2021-10-01 22:45:54
+ * Problem URL: https://atcoder.jp/contests/abc119/tasks/abc119_a
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include<bits/stdc++.h>
+main(){std::string t;std::cin>>t;puts("2019/05/01">t?"Heisei":"TBD");}
