7f95be932aeafb37eda5f7f4879feb6751d17a3a
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 22:55:19 +0900

    [AC] SubmissionID: 26256995 at 2021-10-01 22:55:19 (1633096519)

---
+++ abc122/abc122_a_26256995_AC.cpp
@@ -0,0 +1,29 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc122/submissions/26256995
+ * Submitted at: 2021-10-01 22:55:19
+ * Problem URL: https://atcoder.jp/contests/abc122/tasks/abc122_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  char c;
+  cin>>c;
+  switch(c){
+    case 'A':
+      cout<<"T"<<endl;
+      break;
+    case 'T':
+      cout<<"A"<<endl;
+      break;
+    case 'G':
+      cout<<"C"<<endl;
+      break;
+    case 'C':
+      cout<<"G"<<endl;
+      break;
+  }
+}
