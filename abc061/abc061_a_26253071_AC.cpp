0d23901fbdc7861edab67c85dc2b6c47240d625d
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 18:44:41 +0900

    [AC] SubmissionID: 26253071 at 2021-10-01 18:44:41 (1633081481)

---
+++ abc061/abc061_a_26253071_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc061/submissions/26253071
+ * Submitted at: 2021-10-01 18:44:41
+ * Problem URL: https://atcoder.jp/contests/abc061/tasks/abc061_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int a,b,c;
+  cin>>a>>b>>c;
+  cout<<(a<=c&&c<=b?"Yes":"No")<<endl;
+}
