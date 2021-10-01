af27bee3a16f0e7af49529a572bd9cdcaef90fdd
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-01 19:23:20 +0900

    [AC] SubmissionID: 26253518 at 2021-10-01 19:23:20 (1633083800)

---
+++ abc075/abc075_a_26253518_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc075/submissions/26253518
+ * Submitted at: 2021-10-01 19:23:20
+ * Problem URL: https://atcoder.jp/contests/abc075/tasks/abc075_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include<iostream>
+using namespace std;int main(){int a,b,c;cin>>a>>b>>c;cout<<(a^b^c)<<endl;}
