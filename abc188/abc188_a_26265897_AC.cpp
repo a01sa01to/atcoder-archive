64c2f74cd26f25c2cdac11f0047112fcf96eb975
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-02 15:46:06 +0900

    [AC] SubmissionID: 26265897 at 2021-10-02 15:46:06 (1633157166)

---
+++ abc188/abc188_a_26265897_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc188/submissions/26265897
+ * Submitted at: 2021-10-02 15:46:06
+ * Problem URL: https://atcoder.jp/contests/abc188/tasks/abc188_a
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include<iostream>
+using namespace std;main(){int a,b;cin>>a>>b;puts(min(a,b)+3>max(a,b)?"Yes":"No");}
