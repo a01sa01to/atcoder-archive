4bea9cfd8b03b03d684e79c1dbc73fdd49f99b4f
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-02 10:19:45 +0900

    [AC] SubmissionID: 26261865 at 2021-10-02 10:19:45 (1633137585)

---
+++ abc167/abc167_a_26261865_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc167/submissions/26261865
+ * Submitted at: 2021-10-02 10:19:45
+ * Problem URL: https://atcoder.jp/contests/abc167/tasks/abc167_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<bits/stdc++.h>
+main(){std::string s,t;std::cin>>s>>t;t.pop_back();puts(s==t?"Yes":"No");}
