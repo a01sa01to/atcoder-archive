991b0c851099657d8c7e399c802b32372e48e4d7
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-02 15:16:40 +0900

    [AC] SubmissionID: 26265404 at 2021-10-02 15:16:40 (1633155400)

---
+++ abc179/abc179_a_26265404_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc179/submissions/26265404
+ * Submitted at: 2021-10-02 15:16:40
+ * Problem URL: https://atcoder.jp/contests/abc179/tasks/abc179_a
+ * Result: AC
+ * Execution Time: 10 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;main(){string s;cin>>s;cout<<s<<(s.back()=='s'?"es":"s");}
