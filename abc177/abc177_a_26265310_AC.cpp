30d147740c8c4cb5176c679e290fa415d7e25348
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-02 15:10:36 +0900

    [AC] SubmissionID: 26265310 at 2021-10-02 15:10:36 (1633155036)

---
+++ abc177/abc177_a_26265310_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc177/submissions/26265310
+ * Submitted at: 2021-10-02 15:10:36
+ * Problem URL: https://atcoder.jp/contests/abc177/tasks/abc177_a
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include<iostream>
+main(){int a,b,c;std::cin>>a>>b>>c;puts(a>b*c?"No":"Yes");}
