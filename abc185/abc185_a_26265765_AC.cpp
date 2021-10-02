86e44ac41ee7af2b9df459d8553ef57e32e9ec9d
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-02 15:38:07 +0900

    [AC] SubmissionID: 26265765 at 2021-10-02 15:38:07 (1633156687)

---
+++ abc185/abc185_a_26265765_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc185/submissions/26265765
+ * Submitted at: 2021-10-02 15:38:07
+ * Problem URL: https://atcoder.jp/contests/abc185/tasks/abc185_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include<bits/stdc++.h>
+main(){int a,b,c,d;std::cin>>a>>b>>c>>d;std::cout<<std::min({a,b,c,d});}
