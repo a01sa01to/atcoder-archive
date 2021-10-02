83155d67b792dc8e44d38cf76c514f4189c85a65
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-02 21:01:45 +0900

    [WA] SubmissionID: 26273423 at 2021-10-02 21:01:45 (1633176105)

---
+++ abc221/abc221_a_26273423_WA.cpp
@@ -0,0 +1,12 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc221/submissions/26273423
+ * Submitted at: 2021-10-02 21:01:45
+ * Problem URL: https://atcoder.jp/contests/abc221/tasks/abc221_a
+ * Result: WA
+ * Execution Time: 6 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){int a,b;cin>>a>>b;cout<<pow(32,(a-b))<<endl;}
