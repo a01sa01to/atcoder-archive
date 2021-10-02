5f3ef6db5540ad36ac13ccf4c4fef4e806278ee0
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-02 09:31:25 +0900

    [AC] SubmissionID: 26261411 at 2021-10-02 09:31:25 (1633134685)

---
+++ abc153/abc153_a_26261411_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc153/submissions/26261411
+ * Submitted at: 2021-10-02 09:31:25
+ * Problem URL: https://atcoder.jp/contests/abc153/tasks/abc153_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<iostream>
+main(){int a,b;std::cin>>a>>b;std::cout<<a/b+!!(a%b);}
