9146119515c78cf5ec8ade66383dae2cc951f9b6
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-02 09:52:13 +0900

    [AC] SubmissionID: 26261577 at 2021-10-02 09:52:13 (1633135933)

---
+++ abc159/abc159_a_26261577_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc159/submissions/26261577
+ * Submitted at: 2021-10-02 09:52:13
+ * Problem URL: https://atcoder.jp/contests/abc159/tasks/abc159_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<iostream>
+main(){int a,b;std::cin>>a>>b;std::cout<<(a--*a+b--*b)/2;}
