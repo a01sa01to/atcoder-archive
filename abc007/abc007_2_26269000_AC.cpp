6942c15de98a05c08385fbe5364afb359d82fd08
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-02 18:38:07 +0900

    [AC] SubmissionID: 26269000 at 2021-10-02 18:38:07 (1633167487)

---
+++ abc007/abc007_2_26269000_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc007/submissions/26269000
+ * Submitted at: 2021-10-02 18:38:07
+ * Problem URL: https://atcoder.jp/contests/abc007/tasks/abc007_2
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<bits/stdc++.h>
+main(){std::string a;std::cin>>a;puts(a=="a"?"-1":"a");}
