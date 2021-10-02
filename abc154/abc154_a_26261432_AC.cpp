b47a3f4d86be51f78930aea0e84444202f5e9e65
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-02 09:33:45 +0900

    [AC] SubmissionID: 26261432 at 2021-10-02 09:33:45 (1633134825)

---
+++ abc154/abc154_a_26261432_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc154/submissions/26261432
+ * Submitted at: 2021-10-02 09:33:45
+ * Problem URL: https://atcoder.jp/contests/abc154/tasks/abc154_a
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;main(){string s,t,u;int a,b;cin>>s>>t>>a>>b>>u;s==u?a--:b--;printf("%d %d",a,b);}
