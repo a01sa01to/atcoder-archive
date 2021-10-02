488b434109d76968a3819baddfba765acec4d678
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-02 10:06:02 +0900

    [AC] SubmissionID: 26261721 at 2021-10-02 10:06:02 (1633136762)

---
+++ abc164/abc164_a_26261721_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc164/submissions/26261721
+ * Submitted at: 2021-10-02 10:06:02
+ * Problem URL: https://atcoder.jp/contests/abc164/tasks/abc164_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<iostream>
+main(){int a,b;std::cin>>a>>b;printf("%ssafe",a>b?"":"un");}
