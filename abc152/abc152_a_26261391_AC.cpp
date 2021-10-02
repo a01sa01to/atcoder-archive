ec042d1cb622bf16e3bc458885c99739c31d1a19
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-02 09:29:35 +0900

    [AC] SubmissionID: 26261391 at 2021-10-02 09:29:35 (1633134575)

---
+++ abc152/abc152_a_26261391_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc152/submissions/26261391
+ * Submitted at: 2021-10-02 09:29:35
+ * Problem URL: https://atcoder.jp/contests/abc152/tasks/abc152_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include<iostream>
+main(){int a,b;std::cin>>a>>b;puts(a>b?"No":"Yes");}
