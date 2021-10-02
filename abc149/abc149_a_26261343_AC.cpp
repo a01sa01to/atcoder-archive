fac57dd3d1091c278a85d9338bbae87c266d24d8
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-02 09:23:55 +0900

    [AC] SubmissionID: 26261343 at 2021-10-02 09:23:55 (1633134235)

---
+++ abc149/abc149_a_26261343_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc149/submissions/26261343
+ * Submitted at: 2021-10-02 09:23:55
+ * Problem URL: https://atcoder.jp/contests/abc149/tasks/abc149_a
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include<bits/stdc++.h>
+main(){std::string s,t;std::cin>>s>>t;std::cout<<t<<s;}
