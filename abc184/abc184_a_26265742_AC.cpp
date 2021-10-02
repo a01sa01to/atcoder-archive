c23fee8e2b2ef8ad38749ff0b82d1c03f18e28fd
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-02 15:36:46 +0900

    [AC] SubmissionID: 26265742 at 2021-10-02 15:36:46 (1633156606)

---
+++ abc184/abc184_a_26265742_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc184/submissions/26265742
+ * Submitted at: 2021-10-02 15:36:46
+ * Problem URL: https://atcoder.jp/contests/abc184/tasks/abc184_a
+ * Result: AC
+ * Execution Time: 4 ms
+ */
+
+#include<iostream>
+main(){int a,b,c,d;std::cin>>a>>b>>c>>d;std::cout<<a*d-b*c;}
