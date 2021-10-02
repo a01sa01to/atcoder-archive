249ebcd16031f5795eae39309bafb59b3a2facee
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-02 15:22:22 +0900

    [AC] SubmissionID: 26265498 at 2021-10-02 15:22:22 (1633155742)

---
+++ abc181/abc181_a_26265498_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc181/submissions/26265498
+ * Submitted at: 2021-10-02 15:22:22
+ * Problem URL: https://atcoder.jp/contests/abc181/tasks/abc181_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<iostream>
+main(int a){std::cin>>a;puts(a%2?"Black":"White");}
