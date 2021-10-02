4c22c8580d5cfb05cc539935ec525a54362d9efe
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-02 13:54:15 +0900

    [AC] SubmissionID: 26264237 at 2021-10-02 13:54:15 (1633150455)

---
+++ abc171/abc171_a_26264237_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc171/submissions/26264237
+ * Submitted at: 2021-10-02 13:54:15
+ * Problem URL: https://atcoder.jp/contests/abc171/tasks/abc171_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include<iostream>
+main(){char c;std::cin>>c;puts(c-'a'<0?"A":"a");}
