61b54da9d60d6c2903d24290492c0720866d1fd6
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-02 09:56:01 +0900

    [AC] SubmissionID: 26261606 at 2021-10-02 09:56:01 (1633136161)

---
+++ abc161/abc161_a_26261606_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc161/submissions/26261606
+ * Submitted at: 2021-10-02 09:56:01
+ * Problem URL: https://atcoder.jp/contests/abc161/tasks/abc161_a
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include<iostream>
+main(){int a,b,c;std::cin>>a>>b>>c;printf("%d %d %d",c,a,b);}
