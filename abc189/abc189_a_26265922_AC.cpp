1263ed4950a8def32b49ad1ab48319e9a7550296
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-02 15:47:40 +0900

    [AC] SubmissionID: 26265922 at 2021-10-02 15:47:40 (1633157260)

---
+++ abc189/abc189_a_26265922_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc189/submissions/26265922
+ * Submitted at: 2021-10-02 15:47:40
+ * Problem URL: https://atcoder.jp/contests/abc189/tasks/abc189_a
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include<iostream>
+main(){std::string s;std::cin>>s;puts(s[0]==s[1]&&s[1]==s[2]?"Won":"Lost");}
