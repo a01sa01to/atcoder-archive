b17a5607bfbeb4ed5787592670f9683a734be272
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-02 13:59:08 +0900

    [AC] SubmissionID: 26264285 at 2021-10-02 13:59:08 (1633150748)

---
+++ abc174/abc174_a_26264285_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc174/submissions/26264285
+ * Submitted at: 2021-10-02 13:59:08
+ * Problem URL: https://atcoder.jp/contests/abc174/tasks/abc174_a
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+#include<iostream>
+main(int a){std::cin>>a;puts(a<30?"No":"Yes");}
