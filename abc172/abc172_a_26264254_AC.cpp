2b9e63c6036146282d61d48e0efb3c2f8497bb28
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-02 13:56:16 +0900

    [AC] SubmissionID: 26264254 at 2021-10-02 13:56:16 (1633150576)

---
+++ abc172/abc172_a_26264254_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc172/submissions/26264254
+ * Submitted at: 2021-10-02 13:56:16
+ * Problem URL: https://atcoder.jp/contests/abc172/tasks/abc172_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include<iostream>
+main(int a){std::cin>>a;std::cout<<a+a*a+a*a*a;}
