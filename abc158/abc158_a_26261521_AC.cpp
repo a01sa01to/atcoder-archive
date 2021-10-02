efe12bee809fb3a3763da84819151c02d8a5ee10
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-02 09:46:34 +0900

    [AC] SubmissionID: 26261521 at 2021-10-02 09:46:34 (1633135594)

---
+++ abc158/abc158_a_26261521_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc158/submissions/26261521
+ * Submitted at: 2021-10-02 09:46:34
+ * Problem URL: https://atcoder.jp/contests/abc158/tasks/abc158_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<bits/stdc++.h>
+main(){std::string s;std::cin>>s;puts(s[0]==s[1]&&s[1]==s[2]?"No":"Yes");}
