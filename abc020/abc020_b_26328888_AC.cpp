4d59b9d22a05fa0afd0047743659af81ccea0627
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-03 11:37:07 +0900

    [AC] SubmissionID: 26328888 at 2021-10-03 11:37:07 (1633228627)

---
+++ abc020/abc020_b_26328888_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc020/submissions/26328888
+ * Submitted at: 2021-10-03 11:37:07
+ * Problem URL: https://atcoder.jp/contests/abc020/tasks/abc020_b
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;main(){string a,b;cin>>a>>b;cout<<stoi(a+b)*2<<endl;}
