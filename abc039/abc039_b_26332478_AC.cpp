d00a1a16c76ab55df9b737f5a55165866bdf7844
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-03 15:19:28 +0900

    [AC] SubmissionID: 26332478 at 2021-10-03 15:19:28 (1633241968)

---
+++ abc039/abc039_b_26332478_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc039/submissions/26332478
+ * Submitted at: 2021-10-03 15:19:28
+ * Problem URL: https://atcoder.jp/contests/abc039/tasks/abc039_b
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include<iostream>
+main(){int x,i=1;std::cin>>x;for(;i<1e3;i++)i*i*i*i==x&&std::cout<<i<<"\n";}
