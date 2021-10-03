83176830a07961d7101fb90f682011ee27b24a10
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-03 14:47:06 +0900

    [AC] SubmissionID: 26331919 at 2021-10-03 14:47:06 (1633240026)

---
+++ abc034/abc034_b_26331919_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc034/submissions/26331919
+ * Submitted at: 2021-10-03 14:47:06
+ * Problem URL: https://atcoder.jp/contests/abc034/tasks/abc034_b
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include<iostream>
+main(int n){std::cin>>n;std::cout<<(n%2?n+1:n-1)<<"\n";}
