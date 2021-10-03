35479a5f282b44a6dab69735a5952c62eae6056c
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-03 10:05:08 +0900

    [AC] SubmissionID: 26327211 at 2021-10-03 10:05:08 (1633223108)

---
+++ abc012/abc012_2_26327211_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc012/submissions/26327211
+ * Submitted at: 2021-10-03 10:05:08
+ * Problem URL: https://atcoder.jp/contests/abc012/tasks/abc012_2
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <iostream>
+main(int n){std::cin>>n;printf("%02d:%02d:%02d",n/3600,n/60%60,n%60);}
