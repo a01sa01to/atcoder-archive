fb70847eb911494d4c2215f2a6f6634381e49fef
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-04 23:09:45 +0900

    [AC] SubmissionID: 26357099 at 2021-10-04 23:09:45 (1633356585)

---
+++ abc069/abc069_b_26357099_AC.cpp
@@ -0,0 +1,12 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc069/submissions/26357099
+ * Submitted at: 2021-10-04 23:09:45
+ * Problem URL: https://atcoder.jp/contests/abc069/tasks/abc069_b
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include<iostream>
+using namespace std;
+main(){string s;cin>>s;cout<<s[0]<<s.size()-2<<s.back()<<endl;}
