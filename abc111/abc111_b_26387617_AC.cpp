3e29ae1cd6ac6713dc0098ef763436bd2a204d02
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-06 22:43:25 +0900

    [AC] SubmissionID: 26387617 at 2021-10-06 22:43:25 (1633527805)

---
+++ abc111/abc111_b_26387617_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc111/submissions/26387617
+ * Submitted at: 2021-10-06 22:43:25
+ * Problem URL: https://atcoder.jp/contests/abc111/tasks/abc111_b
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<iostream>
+main(){int n,i=1;std::cin>>n;for(;i<10;i++)if(i*111>=n){std::cout<<i*111<<"\n";return 0;}}
