a86a9222d43c178be22c8b2da68e23a988307c37
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-09 00:13:52 +0900

    [AC] SubmissionID: 26419553 at 2021-10-09 00:13:52 (1633706032)

---
+++ abc127/abc127_b_26419553_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc127/submissions/26419553
+ * Submitted at: 2021-10-09 00:13:52
+ * Problem URL: https://atcoder.jp/contests/abc127/tasks/abc127_b
+ * Result: AC
+ * Execution Time: 10 ms
+ */
+
+#include<iostream>
+main(){int x,y,z;std::cin>>x>>y>>z;for(int i=0;i<10;i++){z=x*z-y;std::cout<<z<<"\n";}}
