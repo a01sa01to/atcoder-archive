4f65bf550c4d6f12ba3356518aea2d394a53fb8e
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-09 22:48:14 +0900

    [AC] SubmissionID: 26467023 at 2021-10-09 22:48:14 (1633787294)

---
+++ abc222/abc222_a_26467023_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc222/submissions/26467023
+ * Submitted at: 2021-10-09 22:48:14
+ * Problem URL: https://atcoder.jp/contests/abc222/tasks/abc222_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<iostream>
+main(){int a;std::cin>>a;printf("%04d\n",a);}
