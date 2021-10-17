0149c8702e8c2bd9d807288140633ee51eae537d
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-17 21:01:30 +0900

    [WA] SubmissionID: 26622723 at 2021-10-17 21:01:30 (1634472090)

---
+++ abc223/abc223_a_26622723_WA.cpp
@@ -0,0 +1,30 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc223/submissions/26622723
+ * Submitted at: 2021-10-17 21:01:30
+ * Problem URL: https://atcoder.jp/contests/abc223/tasks/abc223_a
+ * Result: WA
+ * Execution Time: 10 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ll> ll1d;
+typedef vector<ll1d> ll2d;
+typedef pair<ll, ll> P;
+#define rep(i, n) for (ll i = 0; i < n; i++)
+#define all(vec) vec.begin(), vec.end()
+
+int main() {
+  cout << fixed << setprecision(15);
+  int a;
+  cin >> a;
+  puts(a!=0&&a%100?"No":"Yes");
+  return 0;
+}
