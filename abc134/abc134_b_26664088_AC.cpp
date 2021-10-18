e2f8c549575d1e1eed782ae3eb5b5c1973509411
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-18 12:02:03 +0900

    [AC] SubmissionID: 26664088 at 2021-10-18 12:02:03 (1634526123)

---
+++ abc134/abc134_b_26664088_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc134/submissions/26664088
+ * Submitted at: 2021-10-18 12:02:03
+ * Problem URL: https://atcoder.jp/contests/abc134/tasks/abc134_b
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<iostream>
+using namespace std;
+int main(){
+  int n,d;cin>>n>>d;
+  cout<<(n-1)/(2*d+1)+1<<endl;
+}
