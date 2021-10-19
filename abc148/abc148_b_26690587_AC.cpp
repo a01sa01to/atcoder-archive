bd9ca6aa9e022a48f090621aadf888947462de1e
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-19 23:23:54 +0900

    [AC] SubmissionID: 26690587 at 2021-10-19 23:23:54 (1634653434)

---
+++ abc148/abc148_b_26690587_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc148/submissions/26690587
+ * Submitted at: 2021-10-19 23:23:54
+ * Problem URL: https://atcoder.jp/contests/abc148/tasks/abc148_b
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n;string s,t;cin>>n>>s>>t;
+  for(int i=0;i<n;i++)cout<<s[i]<<t[i];
+  cout<<endl;
+}
