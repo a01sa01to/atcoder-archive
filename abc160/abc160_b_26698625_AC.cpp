676d69b9724fb6b39fcc37714eb958681e4211ba
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-20 15:28:41 +0900

    [AC] SubmissionID: 26698625 at 2021-10-20 15:28:41 (1634711321)

---
+++ abc160/abc160_b_26698625_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc160/submissions/26698625
+ * Submitted at: 2021-10-20 15:28:41
+ * Problem URL: https://atcoder.jp/contests/abc160/tasks/abc160_b
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include<iostream>
+main(int x){std::cin>>x;std::cout<<x/500*1000+x%500/5*5<<"\n";}
