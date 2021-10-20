1f03b0670ba07bacfbd2edcb0928c15afc4ed78c
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-20 15:05:30 +0900

    [AC] SubmissionID: 26698265 at 2021-10-20 15:05:30 (1634709930)

---
+++ abc154/abc154_b_26698265_AC.cpp
@@ -0,0 +1,12 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc154/submissions/26698265
+ * Submitted at: 2021-10-20 15:05:30
+ * Problem URL: https://atcoder.jp/contests/abc154/tasks/abc154_b
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+main(){string s;cin>>s;for(int i=0;i<s.size();i++)cout<<"x";cout<<endl;}
