6054d98d0d8f8b0735b492361ccd1f1626957b27
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-21 19:01:18 +0900

    [WA] SubmissionID: 26716766 at 2021-10-21 19:01:18 (1634810478)

---
+++ abc170/abc170_b_26716766_WA.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc170/submissions/26716766
+ * Submitted at: 2021-10-21 19:01:18
+ * Problem URL: https://atcoder.jp/contests/abc170/tasks/abc170_b
+ * Result: WA
+ * Execution Time: 6 ms
+ */
+
+#include<iostream>
+main(){int a,b;std::cin>>a>>b;puts(2*a<=b&&b<=4*a?"Yes":"No");}
