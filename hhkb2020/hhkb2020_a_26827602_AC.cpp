ba5d71a5773bc3db1313e82d152d82f103acf816
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-26 18:06:18 +0900

    [AC] SubmissionID: 26827602 at 2021-10-26 18:06:18 (1635239178)

---
+++ hhkb2020/hhkb2020_a_26827602_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/hhkb2020/submissions/26827602
+ * Submitted at: 2021-10-26 18:06:18
+ * Problem URL: https://atcoder.jp/contests/hhkb2020/tasks/hhkb2020_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<iostream>
+main(){char s,t;std::cin>>s>>t;printf("%c\n",s=='Y'?t-32:t);}
