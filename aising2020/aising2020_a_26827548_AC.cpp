552046440f885a23f0de3fdedeeebf0e71c44012
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-26 18:01:40 +0900

    [AC] SubmissionID: 26827548 at 2021-10-26 18:01:40 (1635238900)

---
+++ aising2020/aising2020_a_26827548_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/aising2020/submissions/26827548
+ * Submitted at: 2021-10-26 18:01:40
+ * Problem URL: https://atcoder.jp/contests/aising2020/tasks/aising2020_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <iostream>
+main() {int l,r,d;std::cin>>l>>r>>d;std::cout<<r/d-(l-1)/d<<"\n";}
