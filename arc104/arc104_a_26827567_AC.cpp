e5fc2976eba0166f1e23a76cffade09194fbc28d
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-26 18:03:36 +0900

    [AC] SubmissionID: 26827567 at 2021-10-26 18:03:36 (1635239016)

---
+++ arc104/arc104_a_26827567_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/arc104/submissions/26827567
+ * Submitted at: 2021-10-26 18:03:36
+ * Problem URL: https://atcoder.jp/contests/arc104/tasks/arc104_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include<iostream>
+main(){int a,b;std::cin>>a>>b;printf("%d %d\n",(a+b)/2,(a-b)/2);}
