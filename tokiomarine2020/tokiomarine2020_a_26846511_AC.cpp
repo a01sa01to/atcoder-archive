0a7ae0ea19f6d65565a1c87d49471ec32cc10fb4
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-27 22:55:41 +0900

    [AC] SubmissionID: 26846511 at 2021-10-27 22:55:41 (1635342941)

---
+++ tokiomarine2020/tokiomarine2020_a_26846511_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/tokiomarine2020/submissions/26846511
+ * Submitted at: 2021-10-27 22:55:41
+ * Problem URL: https://atcoder.jp/contests/tokiomarine2020/tasks/tokiomarine2020_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  string s;
+  cin>>s;
+  cout<<s.substr(0,3)<<endl;
+}
