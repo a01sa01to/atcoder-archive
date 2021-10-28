a6a2f4968750c83e0639163bb3682df7b84dcab9
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-28 23:13:42 +0900

    [AC] SubmissionID: 26860686 at 2021-10-28 23:13:42 (1635430422)

---
+++ m-solutions2019/m_solutions2019_a_26860686_AC.cpp
@@ -0,0 +1,14 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/m-solutions2019/submissions/26860686
+ * Submitted at: 2021-10-28 23:13:42
+ * Problem URL: https://atcoder.jp/contests/m-solutions2019/tasks/m_solutions2019_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include<iostream>
+using namespace std;
+int main(){
+  int x;cin>>x;cout<<180*(x-2)<<endl;
+}
