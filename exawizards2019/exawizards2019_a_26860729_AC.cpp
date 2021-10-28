d36bf2d551edc7ffd2e72628d01c08db9b55ac0d
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-28 23:16:03 +0900

    [AC] SubmissionID: 26860729 at 2021-10-28 23:16:03 (1635430563)

---
+++ exawizards2019/exawizards2019_a_26860729_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/exawizards2019/submissions/26860729
+ * Submitted at: 2021-10-28 23:16:03
+ * Problem URL: https://atcoder.jp/contests/exawizards2019/tasks/exawizards2019_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include<iostream>
+using namespace std;
+int main(){
+  int a,b,c;cin>>a>>b>>c;
+  cout<<(a==b&&b==c?"Yes":"No")<<endl;
+}
