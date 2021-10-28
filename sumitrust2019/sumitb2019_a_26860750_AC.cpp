907b5ca8fb57481ea314c8d5dfc01eebc8d11c87
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-28 23:17:18 +0900

    [AC] SubmissionID: 26860750 at 2021-10-28 23:17:18 (1635430638)

---
+++ sumitrust2019/sumitb2019_a_26860750_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/sumitrust2019/submissions/26860750
+ * Submitted at: 2021-10-28 23:17:18
+ * Problem URL: https://atcoder.jp/contests/sumitrust2019/tasks/sumitb2019_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<iostream>
+using namespace std;
+int main(){
+  int a,b,c,d;cin>>a>>b>>c>>d;
+  cout<<(a!=c)<<endl;
+}
