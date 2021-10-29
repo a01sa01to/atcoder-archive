71e8e5a01a6585b5cbc4d9eb6c4e85f282c58c6f
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-29 23:52:54 +0900

    [AC] SubmissionID: 26875086 at 2021-10-29 23:52:54 (1635519174)

---
+++ nomura2020/nomura2020_a_26875086_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/nomura2020/submissions/26875086
+ * Submitted at: 2021-10-29 23:52:54
+ * Problem URL: https://atcoder.jp/contests/nomura2020/tasks/nomura2020_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<iostream>
+using namespace std;
+int main(){
+  int a,b,c,d,e;
+  cin>>a>>b>>c>>d>>e;
+  cout<<60*c+d-60*a-b-e<<endl;
+}
