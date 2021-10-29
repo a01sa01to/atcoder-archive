14b4fb3fc407717d1ad3602ca7140c65545a6e8d
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-30 00:00:46 +0900

    [AC] SubmissionID: 26875197 at 2021-10-30 00:00:46 (1635519646)

---
+++ nikkei2019-2-qual/nikkei2019_2_qual_a_26875197_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/nikkei2019-2-qual/submissions/26875197
+ * Submitted at: 2021-10-30 00:00:46
+ * Problem URL: https://atcoder.jp/contests/nikkei2019-2-qual/tasks/nikkei2019_2_qual_a
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include<iostream>
+using namespace std;
+int main(){
+  int x;cin>>x;
+  cout<<--x/2<<endl;
+}
