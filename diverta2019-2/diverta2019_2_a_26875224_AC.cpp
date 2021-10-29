acdb7cba993a717955fddfc6fd33083449430672
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-30 00:02:51 +0900

    [AC] SubmissionID: 26875224 at 2021-10-30 00:02:51 (1635519771)

---
+++ diverta2019-2/diverta2019_2_a_26875224_AC.cpp
@@ -0,0 +1,12 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/diverta2019-2/submissions/26875224
+ * Submitted at: 2021-10-30 00:02:51
+ * Problem URL: https://atcoder.jp/contests/diverta2019-2/tasks/diverta2019_2_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include<iostream>
+using namespace std;
+int main(){int a,b;cin>>a>>b;cout<<a%b<<endl;}
