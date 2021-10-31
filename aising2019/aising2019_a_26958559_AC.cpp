55ac2f95d677f91644d6f511dd0ac2833e5d711c
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-31 21:58:37 +0900

    [AC] SubmissionID: 26958559 at 2021-10-31 21:58:37 (1635685117)

---
+++ aising2019/aising2019_a_26958559_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/aising2019/submissions/26958559
+ * Submitted at: 2021-10-31 21:58:37
+ * Problem URL: https://atcoder.jp/contests/aising2019/tasks/aising2019_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<iostream>
+using namespace std;
+int main(){
+  int a,b,c;
+  cin>>a>>b>>c;
+  cout<<(++a-b)*(a-c)<<endl;
+}
