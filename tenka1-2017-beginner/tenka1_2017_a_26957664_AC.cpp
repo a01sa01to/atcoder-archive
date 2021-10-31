6175e176ad49c884f4d97f0c1457037a5c7af02a
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-31 21:41:01 +0900

    [AC] SubmissionID: 26957664 at 2021-10-31 21:41:01 (1635684061)

---
+++ tenka1-2017-beginner/tenka1_2017_a_26957664_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/tenka1-2017-beginner/submissions/26957664
+ * Submitted at: 2021-10-31 21:41:01
+ * Problem URL: https://atcoder.jp/contests/tenka1-2017-beginner/tasks/tenka1_2017_a
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int c=0;char a;
+  for(int i=0;i<6;i++){cin>>a;c+=a-'0';}
+  cout<<c<<endl;
+}
