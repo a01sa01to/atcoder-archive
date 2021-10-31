b300c45d4118b00e205db2b562e2ca006c341431
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-31 22:15:28 +0900

    [AC] SubmissionID: 26959372 at 2021-10-31 22:15:28 (1635686128)

---
+++ abl/abl_b_26959372_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abl/submissions/26959372
+ * Submitted at: 2021-10-31 22:15:28
+ * Problem URL: https://atcoder.jp/contests/abl/tasks/abl_b
+ * Result: AC
+ * Execution Time: 3 ms
+ */
+
+#include<iostream>
+using namespace std;
+int main(){
+  long a,b,c,d;cin>>a>>b>>c>>d;
+  puts(c<=b&&a<=d?"Yes":"No");
+}
