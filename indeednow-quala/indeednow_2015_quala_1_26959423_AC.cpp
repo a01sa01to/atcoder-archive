daaeb562264eee81dcf6434919baafffe9c80d44
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-10-31 22:16:35 +0900

    [AC] SubmissionID: 26959423 at 2021-10-31 22:16:35 (1635686195)

---
+++ indeednow-quala/indeednow_2015_quala_1_26959423_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/indeednow-quala/submissions/26959423
+ * Submitted at: 2021-10-31 22:16:35
+ * Problem URL: https://atcoder.jp/contests/indeednow-quala/tasks/indeednow_2015_quala_1
+ * Result: AC
+ * Execution Time: 4 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  string s,t;
+  cin>>s>>t;
+  cout<<s.size()*t.size()<<endl;
+}
