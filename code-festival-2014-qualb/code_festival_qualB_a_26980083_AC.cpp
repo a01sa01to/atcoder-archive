a3a7e5d980b95d6f937bc28e42aff2f519946083
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-02 00:47:39 +0900

    [AC] SubmissionID: 26980083 at 2021-11-02 00:47:39 (1635781659)

---
+++ code-festival-2014-qualb/code_festival_qualB_a_26980083_AC.cpp
@@ -0,0 +1,12 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/code-festival-2014-qualb/submissions/26980083
+ * Submitted at: 2021-11-02 00:47:39
+ * Problem URL: https://atcoder.jp/contests/code-festival-2014-qualb/tasks/code_festival_qualB_a
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include<iostream>
+using namespace std;
+int main(){int a,b;cin>>a>>b;cout<<max(a,b)<<endl;}
