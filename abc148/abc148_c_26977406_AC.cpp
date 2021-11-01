1b32a7dbff4a4914d68e4b9824b45f27834e0b82
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-01 22:28:34 +0900

    [AC] SubmissionID: 26977406 at 2021-11-01 22:28:34 (1635773314)

---
+++ abc148/abc148_c_26977406_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc148/submissions/26977406
+ * Submitted at: 2021-11-01 22:28:34
+ * Problem URL: https://atcoder.jp/contests/abc148/tasks/abc148_c
+ * Result: AC
+ * Execution Time: 3 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  long long a,b;cin>>a>>b;
+  cout<<lcm(a,b)<<endl;
+}
