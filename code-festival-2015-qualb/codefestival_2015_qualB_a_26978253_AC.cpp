c89693bb583dcd87b751c42def457716a26ad221
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-01 23:05:03 +0900

    [AC] SubmissionID: 26978253 at 2021-11-01 23:05:03 (1635775503)

---
+++ code-festival-2015-qualb/codefestival_2015_qualB_a_26978253_AC.cpp
@@ -0,0 +1,12 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/code-festival-2015-qualb/submissions/26978253
+ * Submitted at: 2021-11-01 23:05:03
+ * Problem URL: https://atcoder.jp/contests/code-festival-2015-qualb/tasks/codefestival_2015_qualB_a
+ * Result: AC
+ * Execution Time: 3 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+main(){string s;cin>>s;cout<<s<<s<<endl;}
