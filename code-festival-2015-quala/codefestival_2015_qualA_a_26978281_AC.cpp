f9096438481f8362e9410cb3196e3b065494e02b
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-01 23:06:31 +0900

    [AC] SubmissionID: 26978281 at 2021-11-01 23:06:31 (1635775591)

---
+++ code-festival-2015-quala/codefestival_2015_qualA_a_26978281_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/code-festival-2015-quala/submissions/26978281
+ * Submitted at: 2021-11-01 23:06:31
+ * Problem URL: https://atcoder.jp/contests/code-festival-2015-quala/tasks/codefestival_2015_qualA_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  string s;
+  cin>>s;
+  s.back() = '5';
+  cout<<s<<endl;
+}
