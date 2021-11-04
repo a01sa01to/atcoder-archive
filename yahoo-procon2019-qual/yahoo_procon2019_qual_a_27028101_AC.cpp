cfc07a9baebacf74e96c61f00ea1ab71ee5bd572
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-05 01:07:52 +0900

    [AC] SubmissionID: 27028101 at 2021-11-05 01:07:52 (1636042072)

---
+++ yahoo-procon2019-qual/yahoo_procon2019_qual_a_27028101_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/yahoo-procon2019-qual/submissions/27028101
+ * Submitted at: 2021-11-05 01:07:52
+ * Problem URL: https://atcoder.jp/contests/yahoo-procon2019-qual/tasks/yahoo_procon2019_qual_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<iostream>
+using namespace std;
+int main(){
+  int n,k;
+  cin>>n>>k;
+  puts(2*k-1<=n?"YES":"NO");
+}
