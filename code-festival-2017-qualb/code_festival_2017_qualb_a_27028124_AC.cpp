b133e2b166fbbfc4677d854aeba5db9a7babf11e
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-05 01:10:11 +0900

    [AC] SubmissionID: 27028124 at 2021-11-05 01:10:11 (1636042211)

---
+++ code-festival-2017-qualb/code_festival_2017_qualb_a_27028124_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/code-festival-2017-qualb/submissions/27028124
+ * Submitted at: 2021-11-05 01:10:11
+ * Problem URL: https://atcoder.jp/contests/code-festival-2017-qualb/tasks/code_festival_2017_qualb_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  string s;cin>>s;
+  for(int i=0;i<8;i++) s.pop_back();
+  cout<<s<<endl;
+}
