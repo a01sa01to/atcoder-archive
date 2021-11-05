d6571ca6aa3d4a839596878e6b6e00566f6fb6b6
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-05 14:54:27 +0900

    [AC] SubmissionID: 27034658 at 2021-11-05 14:54:27 (1636091667)

---
+++ code-festival-2017-quala/code_festival_2017_quala_a_27034658_AC.cpp
@@ -0,0 +1,14 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/code-festival-2017-quala/submissions/27034658
+ * Submitted at: 2021-11-05 14:54:27
+ * Problem URL: https://atcoder.jp/contests/code-festival-2017-quala/tasks/code_festival_2017_quala_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<bits/stdc++.h>
+main(){
+  std::string s;std::cin>>s;
+  puts(s.substr(0,4)=="YAKI"?"Yes":"No");
+}
