c2910c9180c8b407202652454a24dd502c66134e
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-05 14:51:11 +0900

    [AC] SubmissionID: 27034605 at 2021-11-05 14:51:11 (1636091471)

---
+++ arc027/arc027_1_27034605_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/arc027/submissions/27034605
+ * Submitted at: 2021-11-05 14:51:11
+ * Problem URL: https://atcoder.jp/contests/arc027/tasks/arc027_1
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include<iostream>
+using namespace std;
+int main(){
+  int h,m;cin>>h>>m;
+  cout<<(18-h)*60-m<<endl;
+}
