b464ab283589ad6a6791fd5ff5cc4c87e6a89d7b
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-05 14:36:52 +0900

    [AC] SubmissionID: 27034467 at 2021-11-05 14:36:52 (1636090612)

---
+++ arc053/arc053_a_27034467_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/arc053/submissions/27034467
+ * Submitted at: 2021-11-05 14:36:52
+ * Problem URL: https://atcoder.jp/contests/arc053/tasks/arc053_a
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+#include<iostream>
+using namespace std;
+int main(){
+  int a,b;cin>>a>>b;
+  cout<<(a-1)*b+(b-1)*a<<endl;
+}
