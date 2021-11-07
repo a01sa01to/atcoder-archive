d99a2f9f1ec504740441dc6a876f0ae146680e7f
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-07 10:58:45 +0900

    [AC] SubmissionID: 27064994 at 2021-11-07 10:58:45 (1636250325)

---
+++ arc055/arc055_a_27064994_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/arc055/submissions/27064994
+ * Submitted at: 2021-11-07 10:58:45
+ * Problem URL: https://atcoder.jp/contests/arc055/tasks/arc055_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n;cin>>n;
+  cout<<1;
+  for(int i=0;i<n-1;i++)cout<<0;
+  cout<<7<<endl;
+}
