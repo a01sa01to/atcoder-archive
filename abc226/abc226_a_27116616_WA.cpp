3bfcd656301e9077cae235be16a4005ff946aef8
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-07 23:00:46 +0900

    [WA] SubmissionID: 27116616 at 2021-11-07 23:00:46 (1636293646)

---
+++ abc226/abc226_a_27116616_WA.cpp
@@ -0,0 +1,31 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc226/submissions/27116616
+ * Submitted at: 2021-11-07 23:00:46
+ * Problem URL: https://atcoder.jp/contests/abc226/tasks/abc226_a
+ * Result: WA
+ * Execution Time: 4 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ll> ll1d;
+typedef vector<ll1d> ll2d;
+typedef pair<ll, ll> P;
+typedef long double ld;
+#define rep(i, n) for (ll i = 0; i < n; i++)
+#define all(vec) vec.begin(), vec.end()
+
+int main() {
+  cout << fixed << setprecision(15);
+  double a;
+  cin >> a;
+  cout << round(a) << endl;
+  return 0;
+}
