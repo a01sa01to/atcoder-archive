b59ce9fb4b72fda6bb6e5cd8d89ec8b5061e71ce
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-07 22:23:24 +0900

    [AC] SubmissionID: 27109509 at 2021-11-07 22:23:24 (1636291404)

---
+++ arc033/arc033_1_27109509_AC.cpp
@@ -0,0 +1,12 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/arc033/submissions/27109509
+ * Submitted at: 2021-11-07 22:23:24
+ * Problem URL: https://atcoder.jp/contests/arc033/tasks/arc033_1
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include<iostream>
+using namespace std;
+main(){int n;cin>>n;cout<<n*(n+1)/2<<endl;}
