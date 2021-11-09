bf6341de317496b48693dd3cf42f5d9acc67a29f
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-09 15:05:54 +0900

    [AC] SubmissionID: 27146128 at 2021-11-09 15:05:54 (1636437954)

---
+++ indeednow-qualb/indeednow_2015_qualb_1_27146128_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/indeednow-qualb/submissions/27146128
+ * Submitted at: 2021-11-09 15:05:54
+ * Problem URL: https://atcoder.jp/contests/indeednow-qualb/tasks/indeednow_2015_qualb_1
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int a,b,c,d;cin>>a>>b>>c>>d;
+  cout<<abs(a-c)+abs(b-d)+1<<endl;
+}
