bc34eaf6e075303492d2abc5eb31188de5494763
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-09 12:06:35 +0900

    [AC] SubmissionID: 27144395 at 2021-11-09 12:06:35 (1636427195)

---
+++ arc016/arc016_1_27144395_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/arc016/submissions/27144395
+ * Submitted at: 2021-11-09 12:06:35
+ * Problem URL: https://atcoder.jp/contests/arc016/tasks/arc016_1
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n,x;cin>>n>>x;
+  for(int i=1;i<=n;i++){
+    if(i!=x){cout<<i<<endl;break;}
+  }
+}
