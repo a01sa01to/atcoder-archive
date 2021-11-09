e21f144422a3e196a5d6d9b75e2250aaab27bd58
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-09 13:21:34 +0900

    [AC] SubmissionID: 27145063 at 2021-11-09 13:21:34 (1636431694)

---
+++ arc015/arc015_1_27145063_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/arc015/submissions/27145063
+ * Submitted at: 2021-11-09 13:21:34
+ * Problem URL: https://atcoder.jp/contests/arc015/tasks/arc015_1
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n;cin>>n;
+  double x=1.8*n+32;
+  cout<<x<<endl;
+}
