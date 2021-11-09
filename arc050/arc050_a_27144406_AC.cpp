5300d8e824d41037c190b66d408761ea373116db
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-09 12:07:49 +0900

    [AC] SubmissionID: 27144406 at 2021-11-09 12:07:49 (1636427269)

---
+++ arc050/arc050_a_27144406_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/arc050/submissions/27144406
+ * Submitted at: 2021-11-09 12:07:49
+ * Problem URL: https://atcoder.jp/contests/arc050/tasks/arc050_a
+ * Result: AC
+ * Execution Time: 10 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  char a,b;cin>>a>>b;
+  puts(a-'A'==b-'a'?"Yes":"No");
+}
