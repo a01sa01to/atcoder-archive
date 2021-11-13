44dca5612dbeae7cf945029e75e38c4824c6a884
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-13 11:56:12 +0900

    [AC] SubmissionID: 27200619 at 2021-11-13 11:56:12 (1636772172)

---
+++ arc014/arc014_1_27200619_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/arc014/submissions/27200619
+ * Submitted at: 2021-11-13 11:56:12
+ * Problem URL: https://atcoder.jp/contests/arc014/tasks/arc014_1
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n;cin>>n;
+  puts(n&1?"Red":"Blue");
+}
