6e2128494d181d681bd63d1d75a6373d1919142e
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-13 18:41:19 +0900

    [WA] SubmissionID: 27207527 at 2021-11-13 18:41:19 (1636796479)

---
+++ abc225/abc225_c_27207527_WA.cpp
@@ -0,0 +1,33 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc225/submissions/27207527
+ * Submitted at: 2021-11-13 18:41:19
+ * Problem URL: https://atcoder.jp/contests/abc225/tasks/abc225_c
+ * Result: WA
+ * Execution Time: 33 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n,m;cin>>n>>m;
+  int prevr=-1,prevc=-1;
+  bool chk=true;
+  for(int i=0;i<n;i++){
+    prevc=-1;
+    for(int j=0;j<m;j++){
+      if(prevr==-1){cin>>prevr;prevc=prevr;}
+      else if(prevc==-1){
+        cin>>prevc;
+        if(prevr+7!=prevc)chk=false;
+        prevr=prevc;
+      }
+      else{
+        int a;cin>>a;
+        if(prevc+1!=a)chk=false;
+        prevc=a;
+      }
+    }
+  }
+  puts(chk?"Yes":"No");
+}
