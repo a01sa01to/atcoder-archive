43c302da6a473a3074dc51e70d601e003ac8926b
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-13 15:32:01 +0900

    [AC] SubmissionID: 27203650 at 2021-11-13 15:32:01 (1636785121)

---
+++ donuts-2015/donuts_2015_1_27203650_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/donuts-2015/submissions/27203650
+ * Submitted at: 2021-11-13 15:32:01
+ * Problem URL: https://atcoder.jp/contests/donuts-2015/tasks/donuts_2015_1
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int r,d;cin>>r>>d;
+  cout<<fixed<<setprecision(15)<<r*r*2*d*9.86960440108935<<endl;
+}
