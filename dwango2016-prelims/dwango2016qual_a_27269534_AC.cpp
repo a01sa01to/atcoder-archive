abba86cb0bc028340aa076d190c0ffbb2f44e432
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-15 11:55:22 +0900

    [AC] SubmissionID: 27269534 at 2021-11-15 11:55:22 (1636944922)

---
+++ dwango2016-prelims/dwango2016qual_a_27269534_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/dwango2016-prelims/submissions/27269534
+ * Submitted at: 2021-11-15 11:55:22
+ * Problem URL: https://atcoder.jp/contests/dwango2016-prelims/tasks/dwango2016qual_a
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n;cin>>n;
+  cout<<n/25<<endl;
+}
