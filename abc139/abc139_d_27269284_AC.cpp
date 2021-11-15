690d7cfb98832f43485d37e8f11efede5f6c9a84
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-15 11:31:12 +0900

    [AC] SubmissionID: 27269284 at 2021-11-15 11:31:12 (1636943472)

---
+++ abc139/abc139_d_27269284_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc139/submissions/27269284
+ * Submitted at: 2021-11-15 11:31:12
+ * Problem URL: https://atcoder.jp/contests/abc139/tasks/abc139_d
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  long long n;cin>>n;
+  cout<<n*(n-1)/2<<endl;
+}
