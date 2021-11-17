b4e9fcb867eb48ba7e42f91a113ad92bc0ebc998
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-17 13:25:44 +0900

    [AC] SubmissionID: 27299223 at 2021-11-17 13:25:44 (1637123144)

---
+++ abc157/abc157_c_27299223_AC.cpp
@@ -0,0 +1,23 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc157/submissions/27299223
+ * Submitted at: 2021-11-17 13:25:44
+ * Problem URL: https://atcoder.jp/contests/abc157/tasks/abc157_c
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n,m;cin>>n>>m;
+  vector<int> v(n,-1);
+  for(int i=0;i<m;i++){
+    int a,b;cin>>a>>b;a--;
+    if((n!=1&&a==0&&b==0)||(v[a]!=-1&&v[a]!=b)){cout<<-1<<endl;return 0;}
+    v[a]=b;
+  }
+  cout<<(v[0]==-1?(n==1?0:1):v[0]);
+  for(int i=1;i<n;i++)cout<<(v[i]==-1?0:v[i]);
+  cout<<endl;
+}
