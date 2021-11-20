502cf5ab9fde1841e5f8a9f1549da03283ed0f0c
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-20 23:45:07 +0900

    [AC] SubmissionID: 27406064 at 2021-11-20 23:45:07 (1637419507)

---
+++ arc091/arc091_a_27406064_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/arc091/submissions/27406064
+ * Submitted at: 2021-11-20 23:45:07
+ * Problem URL: https://atcoder.jp/contests/arc091/tasks/arc091_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  long long h,w;cin>>h>>w;
+  cout<<abs((h-2)*(w-2))<<endl;
+}
