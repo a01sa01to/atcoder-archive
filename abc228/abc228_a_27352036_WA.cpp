4d7c32533293df2cce1d42175789a436540ac09a
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-20 21:01:08 +0900

    [WA] SubmissionID: 27352036 at 2021-11-20 21:01:08 (1637409668)

---
+++ abc228/abc228_a_27352036_WA.cpp
@@ -0,0 +1,27 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc228/submissions/27352036
+ * Submitted at: 2021-11-20 21:01:08
+ * Problem URL: https://atcoder.jp/contests/abc228/tasks/abc228_a
+ * Result: WA
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef long double ld;
+#define rep(i, n) for (ll i = 0; i < n; i++)
+
+int main() {
+  int s, t, x;
+  cin >> s >> t >> x;
+  if (t < s) s += 24;
+  puts(s <= x && x < t ? "Yes" : "No");
+  return 0;
+}
