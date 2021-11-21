e474d0c563c683d51f5c20e53f0bd53440fa5074
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-21 22:17:37 +0900

    [AC] SubmissionID: 27429808 at 2021-11-21 22:17:37 (1637500657)

---
+++ abc039/abc039_c_27429808_AC.cpp
@@ -0,0 +1,21 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc039/submissions/27429808
+ * Submitted at: 2021-11-21 22:17:37
+ * Problem URL: https://atcoder.jp/contests/abc039/tasks/abc039_c
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  string s;cin>>s;
+  if(s=="WBWBWWBWBWBWWBWBWWBW")puts("Do");
+  if(s=="WBWWBWBWBWWBWBWWBWBW")puts("Re");
+  if(s=="WWBWBWBWWBWBWWBWBWBW")puts("Mi");
+  if(s=="WBWBWBWWBWBWWBWBWBWW")puts("Fa");
+  if(s=="WBWBWWBWBWWBWBWBWWBW")puts("So");
+  if(s=="WBWWBWBWWBWBWBWWBWBW")puts("La");
+  if(s=="WWBWBWWBWBWBWWBWBWWB")puts("Si");
+}
