3b5c1c1f88ac736850dd31763f83d4b9df6279f5
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-23 11:44:05 +0900

    [WA] SubmissionID: 27454669 at 2021-11-23 11:44:05 (1637635445)

---
+++ abc169/abc169_c_27454669_WA.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc169/submissions/27454669
+ * Submitted at: 2021-11-23 11:44:05
+ * Problem URL: https://atcoder.jp/contests/abc169/tasks/abc169_c
+ * Result: WA
+ * Execution Time: 6 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  long long a,b;double _b;cin>>a>>_b;b=_b*100;
+  cout<<a*b/100<<endl;
+}
