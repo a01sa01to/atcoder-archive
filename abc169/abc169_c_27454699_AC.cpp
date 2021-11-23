8e94b9fc501d62431b16de90b231cf91256d39a7
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-23 11:46:04 +0900

    [AC] SubmissionID: 27454699 at 2021-11-23 11:46:04 (1637635564)

---
+++ abc169/abc169_c_27454699_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc169/submissions/27454699
+ * Submitted at: 2021-11-23 11:46:04
+ * Problem URL: https://atcoder.jp/contests/abc169/tasks/abc169_c
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  long long a,b;int x,y;cin>>a;scanf("%d.%d",&x,&y);b=x*100+y;
+  cout<<a*b/100<<endl;
+}
