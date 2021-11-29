9b728f129c3c13c7c11e725fa4e1fc5acabc8182
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-30 00:40:00 +0900

    [WA] SubmissionID: 27594714 at 2021-11-30 00:40:00 (1638200400)

---
+++ agc035/agc035_a_27594714_WA.cpp
@@ -0,0 +1,26 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/agc035/submissions/27594714
+ * Submitted at: 2021-11-30 00:40:00
+ * Problem URL: https://atcoder.jp/contests/agc035/tasks/agc035_a
+ * Result: WA
+ * Execution Time: 53 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n;cin>>n;
+  if(n%3){puts("No");return 0;}
+  map<int,int>mp;
+  for(int i=0;i<n;++i){int a;cin>>a;++mp[a];}
+  bool chk=false;
+  if(mp.size()==1 && mp[0]==n)chk=true;
+  else if(mp.size()==2 && mp[0]==n/3)chk=true;
+  else if(mp.size()==3){
+    vector<int>t;
+    for(auto x:mp)if(x.second==n/3)t.push_back(x.first);
+    if(t.size()==3 && (t[0]^t[1]==t[2] || t[0]^t[2]==t[1] || t[1]^t[2]==t[0])) chk=true;
+  }
+  puts(chk?"Yes":"No");
+}
