73c580082095dda97fdd0d8cc07b7ace196b6090
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-12-01 23:39:15 +0900

    [WA] SubmissionID: 27619587 at 2021-12-01 23:39:15 (1638369555)

---
+++ agc034/agc034_a_27619587_WA.cpp
@@ -0,0 +1,27 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/agc034/submissions/27619587
+ * Submitted at: 2021-12-01 23:39:15
+ * Problem URL: https://atcoder.jp/contests/agc034/tasks/agc034_a
+ * Result: WA
+ * Execution Time: 15 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n,a,b,c,d;string s;cin>>n>>a>>b>>c>>d>>s;
+  {
+    bool chk=true;
+    for(int i=a;i<max(c,d)-1;++i)if(s[i]=='#'&&s[i+1]=='#')chk=false;
+    if(!chk){puts("No");return 0;}
+  }
+  if(c<d){
+    puts("Yes");
+  }
+  else{
+    bool chk=false;
+    for(int i=b;i<d;++i)if(s[i]=='.'&&s[i+1]=='.'&&s[i+2]=='.')chk=true;
+   	puts(chk?"Yes":"No");
+  }
+}
