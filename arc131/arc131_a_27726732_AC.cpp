ca0861226ebb748330108e82abd8b4eceb2c15e9
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-12-05 23:33:55 +0900

    [AC] SubmissionID: 27726732 at 2021-12-05 23:33:55 (1638714835)

---
+++ arc131/arc131_a_27726732_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/arc131/submissions/27726732
+ * Submitted at: 2021-12-05 23:33:55
+ * Problem URL: https://atcoder.jp/contests/arc131/tasks/arc131_a
+ * Result: AC
+ * Execution Time: 3 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  long a,b;cin>>a>>b;
+  long t=5e8;
+  cout<<a+b*t<<endl;
+}
