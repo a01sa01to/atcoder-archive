c1437090c8049dad63e7b6e158b92cb3a34ea9f4
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-12-11 22:53:26 +0900

    [AC] SubmissionID: 27852921 at 2021-12-11 22:53:26 (1639230806)

---
+++ abc231/abc231_d_27852921_AC.cpp
@@ -0,0 +1,49 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc231/submissions/27852921
+ * Submitted at: 2021-12-11 22:53:26
+ * Problem URL: https://atcoder.jp/contests/abc231/tasks/abc231_d
+ * Result: AC
+ * Execution Time: 66 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef long double ld;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector Graph(n, vector<int>(0));
+  dsu d(n);
+  rep(i, m) {
+    int a, b;
+    cin >> a >> b;
+    --a, --b;
+    if (d.same(a, b)) {
+      puts("No");
+      return 0;
+    }
+    d.merge(a, b);
+    Graph[a].push_back(b);
+    Graph[b].push_back(a);
+  }
+  bool chk = true;
+  int cnt = 0;
+  rep(i, n) {
+    if (Graph[i].size() > 2) {
+      chk = false;
+      break;
+    }
+    if (Graph[i].size() == 2) ++cnt;
+  }
+  puts(chk && cnt <= n - 2 ? "Yes" : "No");
+  return 0;
+}
