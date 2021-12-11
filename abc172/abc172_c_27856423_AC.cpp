2920fc6ac7fef5b5be1e1826e5b5085274feefc9
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-12-12 00:42:07 +0900

    [AC] SubmissionID: 27856423 at 2021-12-12 00:42:07 (1639237327)

---
+++ abc172/abc172_c_27856423_AC.cpp
@@ -0,0 +1,43 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc172/submissions/27856423
+ * Submitted at: 2021-12-12 00:42:07
+ * Problem URL: https://atcoder.jp/contests/abc172/tasks/abc172_c
+ * Result: AC
+ * Execution Time: 123 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef long double ld;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  ll n, m, k;
+  cin >> n >> m >> k;
+  vector<ll> asum(n + 1, 0), bsum(m + 1, 0);
+  rep(i, n) {
+    ll a;
+    cin >> a;
+    asum[i + 1] = asum[i] + a;
+  }
+  rep(i, m) {
+    ll b;
+    cin >> b;
+    bsum[i + 1] = bsum[i] + b;
+  }
+  ll ans = 0;
+  ll bidx = m;
+  rep(i, n + 1) {
+    while (bidx >= 0 && asum[i] + bsum[bidx] > k) --bidx;
+    if (bidx >= 0) ans = max(ans, i + bidx);
+  }
+  cout << ans << endl;
+  return 0;
+}
