46933b5d06e16e38e3a69aab0c1667e2798bd28a
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-12-12 18:18:53 +0900

    [AC] SubmissionID: 27876908 at 2021-12-12 18:18:53 (1639300733)

---
+++ ahc007/ahc007_a_27876908_AC.cpp
@@ -0,0 +1,34 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/ahc007/submissions/27876908
+ * Submitted at: 2021-12-12 18:18:53
+ * Problem URL: https://atcoder.jp/contests/ahc007/tasks/ahc007_a
+ * Result: AC
+ * Execution Time: 44 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef long double ld;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  int n = 400, m = 1995;
+  vector<P> a(n);
+  rep(i, n) cin >> a[i].first >> a[i].second;
+  vector<P> b(m);
+  rep(i, m) cin >> b[i].first >> b[i].second;
+  rep(i, m) {
+    int x;
+    cin >> x;
+    cout << 1 << endl;
+    fflush(stdout);
+  }
+  return 0;
+}
