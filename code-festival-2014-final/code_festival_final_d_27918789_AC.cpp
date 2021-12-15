80c7049624d69008aad69f0d73d19062334491c6
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-12-15 10:57:50 +0900

    [AC] SubmissionID: 27918789 at 2021-12-15 10:57:50 (1639533470)

---
+++ code-festival-2014-final/code_festival_final_d_27918789_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/code-festival-2014-final/submissions/27918789
+ * Submitted at: 2021-12-15 10:57:50
+ * Problem URL: https://atcoder.jp/contests/code-festival-2014-final/tasks/code_festival_final_d
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n;cin>>n;
+  printf("%d 2\n",n+1);
+}
