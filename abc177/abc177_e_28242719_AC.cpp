4bc46299dee68e7871188dff62463d2bce5af42f
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-12-31 00:30:31 +0900

    [AC] SubmissionID: 28242719 at 2021-12-31 00:30:31 (1640878231)

---
+++ abc177/abc177_e_28242719_AC.cpp
@@ -0,0 +1,61 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc177/submissions/28242719
+ * Submitted at: 2021-12-31 00:30:31
+ * Problem URL: https://atcoder.jp/contests/abc177/tasks/abc177_e
+ * Result: AC
+ * Execution Time: 453 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+#else
+  #define Debug(x) 0
+#endif
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef long double ld;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+set<int> s;
+
+bool prime_factorize(int n) {
+  for (int i = 2; i * i <= n; ++i) {
+    if (n % i != 0) continue;
+    if (s.count(i)) return true;
+    s.insert(i);
+    while (n % i == 0) n /= i;
+  }
+  if (n != 1) {
+    if (s.count(n)) return true;
+    s.insert(n);
+  }
+  return false;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  bool pairwise = true;
+  int setwise = -1;
+  rep(i, n) {
+    int a;
+    cin >> a;
+    if (setwise == -1) setwise = a;
+    else
+      setwise = gcd(setwise, a);
+    if (prime_factorize(a)) pairwise = false;
+  }
+  if (pairwise) cout << "pairwise coprime" << endl;
+  else if (setwise == 1)
+    cout << "setwise coprime" << endl;
+  else
+    cout << "not coprime" << endl;
+  return 0;
+}
