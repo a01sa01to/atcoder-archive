cccc90ff02b38d369c24cddedc6f0af0cb485470
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-01-15 21:49:22 +0900

    [WA] SubmissionID: 28553804 at 2022-01-15 21:49:22 (1642250962)

---
+++ abc235/abc235_d_28553804_WA.cpp
@@ -0,0 +1,56 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc235/submissions/28553804
+ * Submitted at: 2022-01-15 21:49:22
+ * Problem URL: https://atcoder.jp/contests/abc235/tasks/abc235_d
+ * Result: WA
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+#else
+  #define Debug(x) 0
+#endif
+typedef long long ll;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  int a, n;
+  cin >> a >> n;
+  set<int> seen;
+  queue<pair<int, int>> q;
+  seen.insert(n);
+  q.push({ n, 0 });
+  while (!q.empty()) {
+    auto [x, y] = q.front();
+    q.pop();
+    if (x == 1) {
+      cout << y << endl;
+      return 0;
+    }
+    if (x % a == 0 && !seen.count(x / a)) {
+      seen.insert(x / a);
+      q.push({ x / a, y + 1 });
+    }
+    if (x >= 10) {
+      string t = to_string(x);
+      if (t[0] != '0') {
+        t.push_back(t[0]);
+        int tmp = stoi(t.substr(1));
+        if (!seen.count(tmp)) {
+          seen.insert(tmp);
+          q.push({ tmp, y + 1 });
+        }
+      }
+    }
+  }
+  cout << -1 << endl;
+  return 0;
+}
