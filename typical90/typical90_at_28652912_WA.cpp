c4edf9f42ba575a3baf2e417f76e04af5c8aa187
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-01-21 00:25:30 +0900

    [WA] SubmissionID: 28652912 at 2022-01-21 00:25:30 (1642692330)

---
+++ typical90/typical90_at_28652912_WA.cpp
@@ -0,0 +1,51 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/typical90/submissions/28652912
+ * Submitted at: 2022-01-21 00:25:30
+ * Problem URL: https://atcoder.jp/contests/typical90/tasks/typical90_at
+ * Result: WA
+ * Execution Time: 99 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+#else
+  #define Debug(x) 0
+#endif
+typedef long long ll;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> a(46), b(46), c(46);
+  rep(i, n) {
+    int x;
+    cin >> x;
+    ++a[x % 46];
+  }
+  rep(i, n) {
+    int x;
+    cin >> x;
+    ++b[x % 46];
+  }
+  rep(i, n) {
+    int x;
+    cin >> x;
+    ++c[x % 46];
+  }
+  ll ans = 0;
+  rep(i, 46) rep(j, 46) rep(k, 46) {
+    if ((i + j + k) % 46 == 0) {
+      ans += a[i] * b[j] * c[k];
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
