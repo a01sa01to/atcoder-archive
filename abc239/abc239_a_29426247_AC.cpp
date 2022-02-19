c05e48c0b8bec3816c85de92db36c5fb4f347980
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-02-19 21:01:32 +0900

    [AC] SubmissionID: 29426247 at 2022-02-19 21:01:32 (1645272092)

---
+++ abc239/abc239_a_29426247_AC.cpp
@@ -0,0 +1,29 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc239/submissions/29426247
+ * Submitted at: 2022-02-19 21:01:32
+ * Problem URL: https://atcoder.jp/contests/abc239/tasks/abc239_a
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+#else
+  #define Debug(x) 0
+#endif
+typedef long long ll;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  long double x;
+  cin >> x;
+  cout << fixed << setprecision(15) << sqrt(x * (1.28e7 + x)) << endl;
+  return 0;
+}
