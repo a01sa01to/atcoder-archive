1a9bc415f7e5ff538cc778cd67fe06b08810699e
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-03-04 00:35:44 +0900

    [AC] SubmissionID: 29830959 at 2022-03-04 00:35:44 (1646321744)

---
+++ iroha2019-day1/iroha2019_day1_c_29830959_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/iroha2019-day1/submissions/29830959
+ * Submitted at: 2022-03-04 00:35:44
+ * Problem URL: https://atcoder.jp/contests/iroha2019-day1/tasks/iroha2019_day1_c
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n;cin>>n;
+  for(int i=n-7;i<=n;++i)cout<<i<<endl;
+  return 0;
+}
