c31cc27c73390292520d8f5d56966df308b169db
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-03-12 22:37:09 +0900

    [WA] SubmissionID: 30077087 at 2022-03-12 22:37:09 (1647092229)

---
+++ abc243/abc243_e_30077087_WA.cpp
@@ -0,0 +1,67 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc243/submissions/30077087
+ * Submitted at: 2022-03-12 22:37:09
+ * Problem URL: https://atcoder.jp/contests/abc243/tasks/abc243_e
+ * Result: WA
+ * Execution Time: 61 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+#else
+  #define Debug(x) 0
+#endif
+typedef long long ll;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector Query(m, tuple<int, int, int>(0, 0, 0));
+  rep(i, m) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    --a, --b;
+    Query[i] = { a, b, c };
+  }
+  sort(Query.begin(), Query.end(), [](auto& a, auto& b) {
+    return get<2>(a) < get<2>(b);
+  });
+
+  vector dist(n, vector<ll>(n, 1e18));
+  int ans = 0;
+  // puts("-----");
+  for (auto [a, b, c] : Query) {
+    if (dist[a][b] <= c) {
+      ++ans;
+      // printf("[DEBUG] %d %d %d\n", a, b, c);
+      continue;
+    }
+    dist[a][b] = dist[b][a] = c;
+    bool flag = true;
+    rep(k, n) {
+      if (dist[a][b] + dist[b][k] < dist[a][k]) {
+        dist[a][k] = dist[k][a] = dist[a][b] + dist[b][k];
+        flag = false;
+      }
+      if (dist[b][a] + dist[a][k] < dist[b][k]) {
+        dist[b][k] = dist[k][b] = dist[b][a] + dist[a][k];
+        flag = false;
+      }
+    }
+    if (flag) {
+      ++ans;
+      // printf("[DEBUG] %d %d %d\n", a, b, c);
+    }
+  }
+
+  cout << ans << endl;
+  return 0;
+}
