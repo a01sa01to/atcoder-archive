e2030dd4d67b18f090b4a36c9be4ce818e78c9e5
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-03-13 10:58:30 +0900

    [AC] SubmissionID: 30093541 at 2022-03-13 10:58:30 (1647136710)

---
+++ abc243/abc243_e_30093541_AC.cpp
@@ -0,0 +1,50 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc243/submissions/30093541
+ * Submitted at: 2022-03-13 10:58:30
+ * Problem URL: https://atcoder.jp/contests/abc243/tasks/abc243_e
+ * Result: AC
+ * Execution Time: 74 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+#else
+  #define Debug(x) 0
+#endif
+typedef long long ll;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  ll n, m;
+  cin >> n >> m;
+  vector Query(m, tuple<ll, ll, ll>(0, 0, 0));
+  rep(i, m) {
+    ll a, b, c;
+    cin >> a >> b >> c;
+    --a, --b;
+    Query[i] = { a, b, c };
+  }
+
+  vector dist(n, vector<ll>(n, 1e18));
+  for (auto [a, b, c] : Query) dist[a][b] = dist[b][a] = c;
+  rep(k, n) rep(i, n) rep(j, n) {
+    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+  }
+
+  ll ans = 0;
+  for (auto [a, b, c] : Query) {
+    bool flag = false;
+    rep(i, n) if (dist[a][i] + dist[i][b] <= c) flag = true;
+    if (flag) ++ans;
+  }
+
+  cout << ans << endl;
+  return 0;
+}
