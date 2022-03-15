a0dcf9ce2737bdbe1cdaaaa5ae5d4dfc57ae5075
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-03-15 22:50:49 +0900

    [AC] SubmissionID: 30151261 at 2022-03-15 22:50:49 (1647352249)

---
+++ jag2017autumn/jag2017autumn_b_30151261_AC.cpp
@@ -0,0 +1,68 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/jag2017autumn/submissions/30151261
+ * Submitted at: 2022-03-15 22:50:49
+ * Problem URL: https://atcoder.jp/contests/jag2017autumn/tasks/jag2017autumn_b
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+#else
+  #define Debug(x) 0
+#endif
+typedef long long ll;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  string s;
+  cin >> s;
+  int n = 0;
+  rep(i, s.size()) if (s[i] != '[' && s[i] != ']' && s[i] != '-')++ n;
+  map<char, int> mp;
+  rep(i, n) {
+    char a;
+    int v;
+    cin >> a >> v;
+    mp[a] = v;
+  }
+  while (s.size() > 1) {
+    string t = "";
+    rep(i, s.size()) {
+      if (i >= s.size() - 4) {
+        t.push_back(s[i]);
+        continue;
+      }
+      string u = s.substr(i, 5);
+      if (u[0] == '[' && u[4] == ']') {
+        if (mp[u[1]] != 0 && mp[u[3]] != 0) {
+          cout << "No" << endl;
+          return 0;
+        }
+        if (mp[u[1]] == 0 && mp[u[3]] == 0) {
+          cout << "No" << endl;
+          return 0;
+        }
+        if (mp[u[1]]--) t.push_back(u[1]);
+        if (mp[u[3]]--) t.push_back(u[3]);
+        i += 4;
+      }
+      else
+        t.push_back(s[i]);
+    }
+    swap(s, t);
+    Debug(s);
+  }
+  if (mp[s[0]] == 0)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+  return 0;
+}
