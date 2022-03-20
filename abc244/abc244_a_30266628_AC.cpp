9c4d91ec35985d48350baf3663111fe2e3bd02ec
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-03-20 21:00:27 +0900

    [AC] SubmissionID: 30266628 at 2022-03-20 21:00:27 (1647777627)

---
+++ abc244/abc244_a_30266628_AC.cpp
@@ -0,0 +1,30 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc244/submissions/30266628
+ * Submitted at: 2022-03-20 21:00:27
+ * Problem URL: https://atcoder.jp/contests/abc244/tasks/abc244_a
+ * Result: AC
+ * Execution Time: 3 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+#else
+  #define Debug(x) 0
+#endif
+typedef long long ll;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  int n;
+  string s;
+  cin >> n >> s;
+  cout << s.back() << endl;
+  return 0;
+}
