09891944012545a5e19de0c5ca7223deda5f1dba
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-03-31 00:41:49 +0900

    [WA] SubmissionID: 30576785 at 2022-03-31 00:41:49 (1648654909)

---
+++ past202005-open/past202005_b_30576785_WA.cpp
@@ -0,0 +1,52 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/past202005-open/submissions/30576785
+ * Submitted at: 2022-03-31 00:41:49
+ * Problem URL: https://atcoder.jp/contests/past202005-open/tasks/past202005_b
+ * Result: WA
+ * Execution Time: 191 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+  #define _GLIBCXX_DEBUG
+#else
+  #define Debug(...) 0
+#endif
+typedef long long ll;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  int n, m, q;
+  cin >> n >> m >> q;
+  vector<int> score(m, n);
+  vector<ll> solved(n, 0);
+  while (q--) {
+    int t;
+    cin >> t;
+    if (t == 1) {
+      int x;
+      cin >> x;
+      --x;
+      int ans = 0;
+      rep(i, m) {
+        if (solved[x] & (1 << i)) ans += score[i];
+      }
+      cout << ans << endl;
+    }
+    else {
+      int x, y;
+      cin >> x >> y;
+      --x, --y;
+      solved[x] |= (1 << y);
+      --score[y];
+    }
+  }
+  return 0;
+}
