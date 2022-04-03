521103d87e8eaf7ed404112a393079e9a4589baa
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-04-03 09:24:01 +0900

    [AC] SubmissionID: 30684660 at 2022-04-03 09:24:01 (1648945441)

---
+++ past202012-open/past202012_c_30684660_AC.js
@@ -0,0 +1,10 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/past202012-open/submissions/30684660
+ * Submitted at: 2022-04-03 09:24:01
+ * Problem URL: https://atcoder.jp/contests/past202012-open/tasks/past202012_c
+ * Result: AC
+ * Execution Time: 244 ms
+ */
+
+console.log((i=>parseInt(i,10).toString(36).toUpperCase())(require("fs").readFileSync("/dev/stdin")))
