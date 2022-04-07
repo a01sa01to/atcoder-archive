64bd82d2fd24f1ff2e0c0247386cd82685f24028
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-04-07 23:58:00 +0900

    [WA] SubmissionID: 30778301 at 2022-04-07 23:58:00 (1649343480)

---
+++ APG4b/APG4b_ct_30778301_WA.py
@@ -0,0 +1,10 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/APG4b/submissions/30778301
+ * Submitted at: 2022-04-07 23:58:00
+ * Problem URL: https://atcoder.jp/contests/APG4b/tasks/APG4b_ct
+ * Result: WA
+ * Execution Time: 22 ms
+ */
+
+print(1/2 * 100 * 101)
