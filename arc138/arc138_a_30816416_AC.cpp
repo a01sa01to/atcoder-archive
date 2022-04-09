837471c03125a003a7485ea356123a6649cfcd40
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-04-09 21:54:06 +0900

    [AC] SubmissionID: 30816416 at 2022-04-09 21:54:06 (1649508846)

---
+++ arc138/arc138_a_30816416_AC.cpp
@@ -0,0 +1,67 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/arc138/submissions/30816416
+ * Submitted at: 2022-04-09 21:54:06
+ * Problem URL: https://atcoder.jp/contests/arc138/tasks/arc138_a
+ * Result: AC
+ * Execution Time: 150 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+  #define _GLIBCXX_DEBUG
+#else
+  #define Debug(...) 0
+#endif
+typedef long long ll;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+  vector<pair<int, int>> a(k);
+  vector<int> b(n - k);
+  rep(i, k) {
+    cin >> a[i].first;
+    a[i].second = i;
+  }
+  rep(i, n - k) cin >> b[i];
+  sort(a.begin(), a.end());
+
+  const int INF = 1e9;
+  int ans = INF;
+  rep(i, n - k) {
+    auto itr = lower_bound(a.begin(), a.end(), make_pair(b[i] - 1, INF));
+    while (itr >= a.begin()) {
+      if (itr->second == INF) break;
+      if (itr->first >= b[i]) {
+        if (itr == a.begin()) break;
+        itr--;
+        continue;
+      }
+      if (itr == a.end()) {
+        itr--;
+        continue;
+      }
+      Debug(*itr, i + k - itr->second);
+      ans = min(ans, i + k - itr->second);
+      itr->second = INF;
+      if (itr == a.begin()) break;
+      itr--;
+    }
+    Debug(ans);
+  }
+
+  if (ans == INF)
+    cout << -1 << endl;
+  else
+    cout << ans << endl;
+
+  return 0;
+}
