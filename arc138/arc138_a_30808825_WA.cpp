8ab2e0286f0af44531b585220733186cb662895a
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-04-09 21:10:01 +0900

    [WA] SubmissionID: 30808825 at 2022-04-09 21:10:01 (1649506201)

---
+++ arc138/arc138_a_30808825_WA.cpp
@@ -0,0 +1,50 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/arc138/submissions/30808825
+ * Submitted at: 2022-04-09 21:10:01
+ * Problem URL: https://atcoder.jp/contests/arc138/tasks/arc138_a
+ * Result: WA
+ * Execution Time: 134 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+  #define _GLIBCXX_DEBUG
+#else
+  #define Debug(...) 0
+#endif
+typedef long long ll;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+  vector<int> a(k);
+  vector<pair<int, int>> b(n - k);
+  rep(i, k) cin >> a[i];
+  rep(i, n - k) {
+    cin >> b[i].first;
+    b[i].second = i + k;
+  }
+
+  const int INF = 1e9;
+  int ans = INF;
+  rep(i, k) {
+    auto itr = upper_bound(b.begin(), b.end(), make_pair(a[i], INF));
+    if (itr == b.end()) continue;
+    ans = min(ans, itr->second - i);
+  }
+
+  if (ans == INF)
+    cout << -1 << endl;
+  else
+    cout << ans << endl;
+
+  return 0;
+}
