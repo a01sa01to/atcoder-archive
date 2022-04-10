9a3ec389e655d6f39b16245e743be67accd12a58
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-04-10 21:06:53 +0900

    [WA] SubmissionID: 30853057 at 2022-04-10 21:06:53 (1649592413)

---
+++ abc247/abc247_b_30853057_WA.cpp
@@ -0,0 +1,54 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc247/submissions/30853057
+ * Submitted at: 2022-04-10 21:06:53
+ * Problem URL: https://atcoder.jp/contests/abc247/tasks/abc247_b
+ * Result: WA
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+  #define _GLIBCXX_DEBUG
+#else
+  #define Debug(...) 0
+#endif
+typedef long long ll;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  int n;
+  cin >> n;
+  vector<pair<string, string>> v(n);
+  rep(i, n) cin >> v[i].first >> v[i].second;
+  set<string> st;
+  rep(i, n) {
+    bool f = true, s = true;
+    if (st.count(v[i].first)) f = false;
+    if (st.count(v[i].second)) s = false;
+    rep(j, n) {
+      if (j <= i) continue;
+      if (v[i].first == v[j].first) f = false;
+      if (v[i].first == v[j].second) f = false;
+      if (v[i].second == v[j].first) s = false;
+      if (v[i].second == v[j].second) s = false;
+    }
+    Debug(v[i], f, s);
+    if (f)
+      st.insert(v[i].first);
+    else if (s)
+      st.insert(v[i].second);
+    else {
+      cout << "No" << endl;
+      return 0;
+    }
+  }
+  cout << "Yes" << endl;
+  return 0;
+}
