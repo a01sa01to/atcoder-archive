4ee163ee207f7f4af1d0c3c24e0662d3f2c3e913
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-04-13 12:20:31 +0900

    [AC] SubmissionID: 30940334 at 2022-04-13 12:20:31 (1649820031)

---
+++ math-and-algorithm/math_and_algorithm_d_30940334_AC.cpp
@@ -0,0 +1,19 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/math-and-algorithm/submissions/30940334
+ * Submitted at: 2022-04-13 12:20:31
+ * Problem URL: https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_d
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  vector<int> a(3);
+  rep(i, 3) cin >> a[i];
+  cout << a[0] * a[1] * a[2] << endl;
+  return 0;
+}
