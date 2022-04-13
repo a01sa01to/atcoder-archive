2c14f63a321d91bd4930f7d0c7e9a63fe408ee82
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-04-13 12:22:19 +0900

    [AC] SubmissionID: 30940354 at 2022-04-13 12:22:19 (1649820139)

---
+++ math-and-algorithm/math_and_algorithm_f_30940354_AC.cpp
@@ -0,0 +1,18 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/math-and-algorithm/submissions/30940354
+ * Submitted at: 2022-04-13 12:22:19
+ * Problem URL: https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_f
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  cout << 2 * n + 3 << endl;
+  return 0;
+}
