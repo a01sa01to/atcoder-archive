e4f963a9e4dca56675b085e81d5b401139513916
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-05-14 21:00:22 +0900

    [WA] SubmissionID: 31654014 at 2022-05-14 21:00:22 (1652529622)

---
+++ abc251/abc251_a_31654014_WA.cpp
@@ -0,0 +1,30 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc251/submissions/31654014
+ * Submitted at: 2022-05-14 21:00:22
+ * Problem URL: https://atcoder.jp/contests/abc251/tasks/abc251_a
+ * Result: WA
+ * Execution Time: 15 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+  #define _GLIBCXX_DEBUG
+#else
+  #define Debug(...) 0
+#endif
+typedef long long ll;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  string s;
+  cin >> s;
+  cout << s << s << endl;
+  return 0;
+}
