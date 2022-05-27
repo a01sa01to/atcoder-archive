c9cb4a3a231924b2071ed5072c7dc99cecfda611
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-05-27 10:19:49 +0900

    [AC] SubmissionID: 31971079 at 2022-05-27 10:19:49 (1653614389)

---
+++ arc112/arc112_b_31971079_AC.cpp
@@ -0,0 +1,46 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/arc112/submissions/31971079
+ * Submitted at: 2022-05-27 10:19:49
+ * Problem URL: https://atcoder.jp/contests/arc112/tasks/arc112_b
+ * Result: AC
+ * Execution Time: 11 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+  #define _GLIBCXX_DEBUG
+#else
+  #define Debug(...) 0
+#endif
+typedef long long ll;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+pair<ll, ll> ans(ll b, ll c) {
+  if (c == 0) {
+    return { b, b };
+  }
+  else if (c % 2) {
+    ll n = (c - 1) / 2;
+    return { -b - n, -b + n };
+  }
+  else {
+    ll n = c / 2;
+    return { b - n, b + n - 1 };
+  }
+}
+
+int main() {
+  ll b, c;
+  cin >> b >> c;
+  auto [w, x] = ans(b, c);
+  auto [y, z] = ans(b, c - 1);
+  cout << (x - w + 1) + (z - y + 1) - max(0LL, min(x, z) - max(w, y) + 1) << endl;
+  return 0;
+}
