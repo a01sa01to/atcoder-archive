e7097534ef75e9b2ae7ad6640d9a897ff4fa9b50
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-05-30 00:51:28 +0900

    [AC] SubmissionID: 32097202 at 2022-05-30 00:51:28 (1653839488)

---
+++ njpc2017/njpc2017_a_32097202_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/njpc2017/submissions/32097202
+ * Submitted at: 2022-05-30 00:51:28
+ * Problem URL: https://atcoder.jp/contests/njpc2017/tasks/njpc2017_a
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n;cin>>n;
+  string s;cin>>s;
+  cout<<s.substr(0,n)<<endl;
+}
