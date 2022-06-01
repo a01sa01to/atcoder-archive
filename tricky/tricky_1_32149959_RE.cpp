41648866e8211b7182f01855c191d54395a77502
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-06-02 01:15:44 +0900

    [RE] SubmissionID: 32149959 at 2022-06-02 01:15:44 (1654100144)

---
+++ tricky/tricky_1_32149959_RE.cpp
@@ -0,0 +1,19 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/tricky/submissions/32149959
+ * Submitted at: 2022-06-02 01:15:44
+ * Problem URL: https://atcoder.jp/contests/tricky/tasks/tricky_1
+ * Result: RE
+ * Execution Time: 1342 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+  int t;cin>>t;
+  while(t--){
+    ll a,b;cin>>a>>b;
+    cout<<a/b<<endl;
+  }
+}
