06afdd137e126acb758f0c212c3b1b5d87a44d0f
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-06-16 00:05:35 +0900

    [AC] SubmissionID: 32491119 at 2022-06-16 00:05:35 (1655305535)

---
+++ pakencamp-2020-day2/pakencamp_2020_day2_a_32491119_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/pakencamp-2020-day2/submissions/32491119
+ * Submitted at: 2022-06-16 00:05:35
+ * Problem URL: https://atcoder.jp/contests/pakencamp-2020-day2/tasks/pakencamp_2020_day2_a
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int a,b;cin>>a>>b;
+  printf("%d %d\n",max(a,b),a+b);
+}
