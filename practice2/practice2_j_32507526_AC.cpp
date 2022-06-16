0843e1d8162020739f0da030a5b70ff83659e30f
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-06-17 00:30:47 +0900

    [AC] SubmissionID: 32507526 at 2022-06-17 00:30:47 (1655393447)

---
+++ practice2/practice2_j_32507526_AC.cpp
@@ -0,0 +1,57 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/practice2/submissions/32507526
+ * Submitted at: 2022-06-17 00:30:47
+ * Problem URL: https://atcoder.jp/contests/practice2/tasks/practice2_j
+ * Result: AC
+ * Execution Time: 393 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+  #define _GLIBCXX_DEBUG
+#else
+  #define Debug(...) 0
+#endif
+typedef long long ll;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int op(int a, int b) {
+  return max(a, b);
+}
+int e() {
+  return -1;
+}
+
+int main() {
+  int n, q;
+  cin >> n >> q;
+  segtree<int, op, e> seg(n);
+  rep(i, n) {
+    int x;
+    cin >> x;
+    seg.set(i, x);
+  }
+  while (q--) {
+    int t, x, y;
+    cin >> t >> x >> y;
+    if (t == 1) {
+      seg.set(--x, y);
+    }
+    else if (t == 2) {
+      cout << seg.prod(--x, y) << endl;
+    }
+    else {
+      cout << 1 + seg.max_right(--x, [&](int a) {
+        return a < y;
+      }) << endl;
+    }
+  }
+  return 0;
+}
