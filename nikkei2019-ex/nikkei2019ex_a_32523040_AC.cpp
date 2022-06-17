930aa4f2340460af87386bf67bf07a3a93b88b40
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-06-18 02:11:33 +0900

    [AC] SubmissionID: 32523040 at 2022-06-18 02:11:33 (1655485893)

---
+++ nikkei2019-ex/nikkei2019ex_a_32523040_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/nikkei2019-ex/submissions/32523040
+ * Submitted at: 2022-06-18 02:11:33
+ * Problem URL: https://atcoder.jp/contests/nikkei2019-ex/tasks/nikkei2019ex_a
+ * Result: AC
+ * Execution Time: 164 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  string s;cin>>s;
+  for(int i=1;i<=s.size();i++) cout<<i<<endl;
+  return 0;
+}
