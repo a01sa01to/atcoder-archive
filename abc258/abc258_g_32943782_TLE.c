5197f7639e97c6e5d90638c28f73f4a102497965
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-07-03 01:55:06 +0900

    [TLE] SubmissionID: 32943782 at 2022-07-03 01:55:06 (1656780906)

---
+++ abc258/abc258_g_32943782_TLE.c
@@ -0,0 +1,30 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc258/submissions/32943782
+ * Submitted at: 2022-07-03 01:55:06
+ * Problem URL: https://atcoder.jp/contests/abc258/tasks/abc258_g
+ * Result: TLE
+ * Execution Time: 3309 ms
+ */
+
+#include <stdio.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int a[3001][3001];
+int main() {
+  int n;scanf("%d",&n);
+  rep(i, n){
+    char c[n]; scanf("%s",c);
+    rep(j, n) a[i][j] = c[j] - '0';
+  }
+  long ans = 0;
+  for (int i = 0; i < n - 2; i++) {
+    for (int j = i + 1; j < n - 1; j++) {
+      for (int k = j + 1; k < n; k++) {
+        if (a[i][j] && a[i][k] && a[j][k]) ++ans;
+      }
+    }
+  }
+  printf("%ld\n",ans);
+  return 0;
+}
