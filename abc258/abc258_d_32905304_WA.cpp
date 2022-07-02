ef246e5a4c388000a3e1f52cccf7976f4f73e068
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-07-02 21:30:33 +0900

    [WA] SubmissionID: 32905304 at 2022-07-02 21:30:33 (1656765033)

---
+++ abc258/abc258_d_32905304_WA.cpp
@@ -0,0 +1,40 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc258/submissions/32905304
+ * Submitted at: 2022-07-02 21:30:33
+ * Problem URL: https://atcoder.jp/contests/abc258/tasks/abc258_d
+ * Result: WA
+ * Execution Time: 127 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+  #define _GLIBCXX_DEBUG
+#else
+  #define Debug(...) 0
+#endif
+typedef long long ll;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  int n, x;
+  cin >> n >> x;
+  vector<pair<ll, ll>> v(n);
+  rep(i, n) cin >> v[i].first >> v[i].second;
+  ll ans = 1e18;
+  ll sum = 0;
+  rep(i, n) {
+    sum += v[i].first;
+    sum += v[i].second;
+    --x;
+    ans = min(ans, sum + x * v[i].second);
+  }
+  cout << ans << endl;
+  return 0;
+}
