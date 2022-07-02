d67248f88cb754c8c72ab5e63ed8acb214244295
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-07-02 22:32:12 +0900

    [TLE] SubmissionID: 32929248 at 2022-07-02 22:32:12 (1656768732)

---
+++ abc258/abc258_g_32929248_TLE.cpp
@@ -0,0 +1,50 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc258/submissions/32929248
+ * Submitted at: 2022-07-02 22:32:12
+ * Problem URL: https://atcoder.jp/contests/abc258/tasks/abc258_g
+ * Result: TLE
+ * Execution Time: 3308 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+  #define _GLIBCXX_DEBUG
+#else
+  #define Debug(...) 0
+#endif
+typedef long long ll;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+
+int main() {
+  ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector a(n, vector<bool>(n));
+  rep(i, n) rep(j, n) {
+    char c;
+    cin >> c;
+    a[i][j] = (c == '1');
+  }
+  ll ans = 0;
+  for (int i = 0; i < n - 2; i++) {
+    for (int j = i + 1; j < n - 1; j++) {
+      for (int k = j + 1; k < n; k++) {
+        if (a[i][j] && a[i][k] && a[j][k]) ++ans;
+      }
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
