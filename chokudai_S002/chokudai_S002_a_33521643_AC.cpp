30f9568756fad5b93e7090780ccc765efba95823
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-07-26 01:26:13 +0900

    [AC] SubmissionID: 33521643 at 2022-07-26 01:26:13 (1658766373)

---
+++ chokudai_S002/chokudai_S002_a_33521643_AC.cpp
@@ -0,0 +1,18 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/chokudai_S002/submissions/33521643
+ * Submitted at: 2022-07-26 01:26:13
+ * Problem URL: https://atcoder.jp/contests/chokudai_S002/tasks/chokudai_S002_a
+ * Result: AC
+ * Execution Time: 427 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n;cin>>n;
+  while(n--){
+    long a,b;cin>>a>>b;
+    cout<<a*b<<endl;
+  }
+}
