691d9debdce9fbe8b65126481ab26b0b688706a9
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-07-28 00:00:16 +0900

    [AC] SubmissionID: 33557705 at 2022-07-28 00:00:16 (1658934016)

---
+++ chokudai_S002/chokudai_S002_b_33557705_AC.cpp
@@ -0,0 +1,19 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/chokudai_S002/submissions/33557705
+ * Submitted at: 2022-07-28 00:00:16
+ * Problem URL: https://atcoder.jp/contests/chokudai_S002/tasks/chokudai_S002_b
+ * Result: AC
+ * Execution Time: 406 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n;cin>>n;
+  while(n--){
+    int a,b;cin>>a>>b;
+    cout<<a%b<<endl;
+  }
+  return 0;
+}
