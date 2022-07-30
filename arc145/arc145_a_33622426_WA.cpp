b5ad8ddae3dc4a34be8eb9f866d9946e92e07a37
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-07-30 21:41:11 +0900

    [WA] SubmissionID: 33622426 at 2022-07-30 21:41:11 (1659184871)

---
+++ arc145/arc145_a_33622426_WA.cpp
@@ -0,0 +1,45 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/arc145/submissions/33622426
+ * Submitted at: 2022-07-30 21:41:11
+ * Problem URL: https://atcoder.jp/contests/arc145/tasks/arc145_a
+ * Result: WA
+ * Execution Time: 16 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+  #define _GLIBCXX_DEBUG
+#else
+  #define Debug(...) 0
+#endif
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  int n;
+  string s;
+  cin >> n >> s;
+  if (s[0] == 'B') {
+    puts("Yes");
+  }
+  else if (s == "BA") {
+    puts("No");
+  }
+  else if (s.back() == 'B') {
+    puts("No");
+  }
+  else if (s[1] == 'B' && n == 4) {
+    puts("No");
+  }
+  else {
+    puts("Yes");
+  }
+  return 0;
+}
