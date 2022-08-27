ff1bed7696cd06630526fdbb88a804659552dc80
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-08-28 00:24:08 +0900

    [AC] SubmissionID: 34409945 at 2022-08-28 00:24:08 (1661613848)

---
+++ abc266/abc266_a_34409945_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc266/submissions/34409945
+ * Submitted at: 2022-08-28 00:24:08
+ * Problem URL: https://atcoder.jp/contests/abc266/tasks/abc266_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  string s;cin>>s;
+  cout<<s[s.size() / 2] << endl;
+}
