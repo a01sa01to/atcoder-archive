c5631ebbfba40c4b83965099c7e4871963c819eb
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-09-05 00:17:56 +0900

    [AC] SubmissionID: 34622887 at 2022-09-05 00:17:56 (1662304676)

---
+++ abc115/abc115_d_34622887_AC.cpp
@@ -0,0 +1,47 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc115/submissions/34622887
+ * Submitted at: 2022-09-05 00:17:56
+ * Problem URL: https://atcoder.jp/contests/abc115/tasks/abc115_d
+ * Result: AC
+ * Execution Time: 11 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+  #define _GLIBCXX_DEBUG
+#else
+  #define Debug(...) 0
+#endif
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+vector<ll> a, p;
+map<pair<ll, ll>, ll> memo;
+ll solve(ll n, ll x) {
+  if (memo.count({ n, x })) return memo[{ n, x }];
+  if (n == 0) return memo[{ n, x }] = x > 0;
+  else if (x <= a[n - 1] + 1)
+    return memo[{ n, x }] = solve(n - 1, x - 1);
+  else
+    return memo[{ n, x }] = p[n - 1] + 1 + solve(n - 1, x - a[n - 1] - 2);
+}
+
+int main() {
+  ll n, x;
+  cin >> n >> x;
+  a.push_back(1), p.push_back(1);
+  rep(i, n) {
+    a.push_back(a.back() * 2 + 3);
+    p.push_back(p.back() * 2 + 1);
+  }
+
+  cout << solve(n, x) << endl;
+  return 0;
+}
