613f9545032bda936b3b84270b9900a2a971168a
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-09-24 22:20:53 +0900

    [AC] SubmissionID: 35130305 at 2022-09-24 22:20:53 (1664025653)

---
+++ abc270/abc270_e_35130305_AC.cpp
@@ -0,0 +1,80 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc270/submissions/35130305
+ * Submitted at: 2022-09-24 22:20:53
+ * Problem URL: https://atcoder.jp/contests/abc270/tasks/abc270_e
+ * Result: AC
+ * Execution Time: 63 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+  #define _GLIBCXX_DEBUG
+#else
+  #define Debug(...) 0
+#endif
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  ll n, k;
+  cin >> n >> k;
+  priority_queue<ll, vector<ll>, greater<ll>> q;
+  vector<ll> a(n);
+  rep(i, n) {
+    cin >> a[i];
+    q.push(a[i]);
+  }
+  ll used = 0, m = 0;
+  while (used < k) {
+    ll x = q.top();
+    if (k - used < (x - m) * q.size()) {
+      break;
+    }
+    used += (x - m) * q.size();
+    m = x;
+    q.pop();
+  }
+  rep(i, n) {
+    if (a[i] <= m) {
+      a[i] = 0;
+    }
+    else {
+      a[i] -= m;
+    }
+  }
+  Debug(used);
+  if (!q.empty()) {
+    ll rem = k - used;
+    ll x = rem / q.size();
+    Debug(rem, x);
+    rep(i, n) {
+      if (a[i] > 0) {
+        assert(a[i] >= x || a[i] == 0);
+        a[i] -= x;
+        rem -= x;
+      }
+    }
+    Debug(rem);
+    rep(i, n) {
+      if (rem == 0) {
+        break;
+      }
+      if (a[i] > 0) {
+        a[i]--;
+        rem--;
+      }
+    }
+  }
+  rep(i, n) {
+    cout << a[i] << " ";
+  }
+  cout << endl;
+  return 0;
+}
