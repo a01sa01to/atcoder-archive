0c237d2bc4cb70a82d47326f08f063c2fa9c0efb
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-10-11 16:42:15 +0900

    [WA] SubmissionID: 35584250 at 2022-10-11 16:42:15 (1665474135)

---
+++ abc272/abc272_d_35584250_WA.cpp
@@ -0,0 +1,52 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc272/submissions/35584250
+ * Submitted at: 2022-10-11 16:42:15
+ * Problem URL: https://atcoder.jp/contests/abc272/tasks/abc272_d
+ * Result: WA
+ * Execution Time: 329 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+  #define _GLIBCXX_DEBUG
+#else
+  #define Debug(...) 0
+#endif
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  const int sqrtM = sqrt(m);
+  vector ans(n, vector(n, -1));
+  queue<pair<int, int>> q;
+  ans[0][0] = 0;
+  q.push({ 0, 0 });
+  while (!q.empty()) {
+    auto [x, y] = q.front();
+    q.pop();
+    for (int dx = -x; x + dx < n; ++dx) {
+      if (dx * dx > m) continue;
+      double dy = sqrt(double(m) - dx * dx);
+      if (dy != floor(dy)) continue;
+      for (int ddy : { (int) dy, -(int) dy }) {
+        if (ans[x + dx][y + ddy] != -1) continue;
+        ans[x + dx][y + ddy] = ans[x][y] + 1;
+        q.push({ x + dx, y + ddy });
+      }
+    }
+  }
+  rep(i, n) {
+    rep(j, n) cout << ans[i][j] << " ";
+    cout << endl;
+  }
+  return 0;
+}
