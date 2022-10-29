2cf78c1d057a292aa42fc0e88b957a0ad93a851f
Asato Tsuchiya <26807394+a01sa01to@users.noreply.github.com>
2022-10-29 21:00:46 +0900

    [AC] SubmissionID: 36032740 at 2022-10-29 21:00:46 (1667044846)

---
+++ abc275/abc275_a_36032740_AC.cpp
@@ -0,0 +1,32 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc275/submissions/36032740
+ * Submitted at: 2022-10-29 21:00:46
+ * Problem URL: https://atcoder.jp/contests/abc275/tasks/abc275_a
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+  #define _GLIBCXX_DEBUG
+#else
+  #define Debug(...) 0
+#endif
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> h(n);
+  rep(i, n) cin >> h[i];
+  cout << (max_element(h.begin(), h.end()) - h.begin()) + 1 << endl;
+  return 0;
+}
