201f0953f26bdb7f1a11b47be1878bec6b55b5d1
Asato Tsuchiya <26807394+a01sa01to@users.noreply.github.com>
2022-11-20 00:13:45 +0900

    [WA] SubmissionID: 36650142 at 2022-11-20 00:13:45 (1668870825)

---
+++ abc278/abc278_f_36650142_WA.cpp
@@ -0,0 +1,42 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc278/submissions/36650142
+ * Submitted at: 2022-11-20 00:13:45
+ * Problem URL: https://atcoder.jp/contests/abc278/tasks/abc278_f
+ * Result: WA
+ * Execution Time: 28 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+  #define _GLIBCXX_DEBUG
+#else
+  #define Debug(...) 0
+#endif
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  int n;
+  cin >> n;
+  vector<string> s(n);
+  rep(i, n) cin >> s[i];
+  vector dp(1 << n, vector<bool>(26, false));
+  rep(bit, 1 << n) {
+    if (bit == 0) continue;
+    rep(i, n) {
+      if (!(bit & (1 << i))) continue;
+      dp[bit][s[i].back() - 'a'] = !dp[bit & ~(1 << i)][s[i].front() - 'a'];
+    }
+  }
+  bool ok = false;
+  rep(i, 26) ok |= dp[(1 << n) - 1][i];
+  cout << (ok ? "First" : "Second") << endl;
+  return 0;
+}
