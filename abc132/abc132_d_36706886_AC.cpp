07bcc3ae37086cfbe140b5a1771bba73facb6a6a
Asato Tsuchiya <26807394+a01sa01to@users.noreply.github.com>
2022-11-22 00:49:40 +0900

    [AC] SubmissionID: 36706886 at 2022-11-22 00:49:40 (1669045780)

---
+++ abc132/abc132_d_36706886_AC.cpp
@@ -0,0 +1,43 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc132/submissions/36706886
+ * Submitted at: 2022-11-22 00:49:40
+ * Problem URL: https://atcoder.jp/contests/abc132/tasks/abc132_d
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+  #define _GLIBCXX_DEBUG
+#else
+  #define Debug(...) 0
+#endif
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+using mint = modint1000000007;
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+  vector<mint> fact(n + 1, 1);
+  rep(i, n) fact[i + 1] = fact[i] * (i + 1);
+  for (int i = 1; i <= k; i++) {
+    // ans = Comb(n-k+1, i) * Comb(k-1, i-1);
+    if (n - k + 1 - i < 0) {
+      cout << 0 << endl;
+      continue;
+    }
+    mint ans = fact[n - k + 1] / fact[i] / fact[n - k + 1 - i];
+    ans *= fact[k - 1] / fact[i - 1] / fact[k - i];
+    cout << ans.val() << endl;
+  }
+  return 0;
+}
