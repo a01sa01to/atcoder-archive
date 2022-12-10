14b06e051e045f1807061bcaa9b142eb5ef9ab33
Asato Tsuchiya <26807394+a01sa01to@users.noreply.github.com>
2022-12-10 21:06:04 +0900

    [AC] SubmissionID: 37143028 at 2022-12-10 21:06:04 (1670673964)

---
+++ abc281/abc281_a_37143028_AC.cpp
@@ -0,0 +1,33 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/abc281/submissions/37143028
+ * Submitted at: 2022-12-10 21:06:04
+ * Problem URL: https://atcoder.jp/contests/abc281/tasks/abc281_a
+ * Result: AC
+ * Execution Time: 4 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+  #define _GLIBCXX_DEBUG
+#else
+  #define Debug(...) 0
+#endif
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  int n;
+  cin >> n;
+  while (n--) {
+    cout << n + 1 << endl;
+  }
+  cout << 0 << endl;
+  return 0;
+}
