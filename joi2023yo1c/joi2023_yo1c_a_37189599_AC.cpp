dc389b65d8bcf83212496d98f70a587ae0a14aa7
Asato Tsuchiya <26807394+a01sa01to@users.noreply.github.com>
2022-12-11 00:05:31 +0900

    [AC] SubmissionID: 37189599 at 2022-12-11 00:05:31 (1670684731)

---
+++ joi2023yo1c/joi2023_yo1c_a_37189599_AC.cpp
@@ -0,0 +1,30 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://atcoder.jp/contests/joi2023yo1c/submissions/37189599
+ * Submitted at: 2022-12-11 00:05:31
+ * Problem URL: https://atcoder.jp/contests/joi2023yo1c/tasks/joi2023_yo1c_a
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+  #define _GLIBCXX_DEBUG
+#else
+  #define Debug(...) 0
+#endif
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  char a, b;
+  cin >> a >> b;
+  cout << a << b << endl;
+  return 0;
+}
